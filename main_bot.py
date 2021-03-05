4efeb6be5446d1a1cd8563027ca74559463488d1
Agustian <agustian@loratechai.com>
2021-03-05 14:57:56 +0700

    Test Infer

--- main_bot.py
+++ main_bot.py
@@ -1,5 +1,8 @@
-from main_executive import data_prep_history, infer_history
+from general.sql_process import do_function
+from main_executive import data_prep_history, infer_history, train_model
 
 if __name__ == "__main__":
+    # do_function("data_vol_surface_update")
     # data_prep_history()
+    train_model()
     infer_history()
