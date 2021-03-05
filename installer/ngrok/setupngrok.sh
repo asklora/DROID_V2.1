28e24ce61c09f93b478252bf1b41f77c52cc2e6b
redloratech <rede.akbar@loratechai.com>
2021-03-05 10:00:47 +0700

    add cloud script

---
+++ installer/ngrok/setupngrok.sh
@@ -0,0 +1,33 @@
+# !/bin/bash
+# download
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+
+# unzip
+unzip ngrok-stable-linux-amd64.zip 
+
+# move to /usr/local/bin
+sudo mv ngrok /usr/local/bin
+
+# create directory
+sudo mkdir -p /opt/ngrok/systemd/system/
+setup="$(pwd)/installer/ngrok/config/$1/ngrok.yml"
+# create systemd script
+echo "[Unit]
+Description=ngrok script
+[Service]
+ExecStart=/usr/local/bin/ngrok --all --region=ap --config=$setup
+Restart=always
+[Install]
+WantedBy=multi-user.target" 
+
+# copy systemd script 
+sudo cp /opt/ngrok/systemd/system/ngrok.service /etc/systemd/system/ngrok.service
+
+# reload daemon
+sudo systemctl daemon-reload
+
+# enable service
+sudo systemctl enable ngrok.service
+
+# start service
+sudo systemctl start ngrok.service
---
+++ installer/ngrok/uninstall-ngrok.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+systemctl stop ngrok.service
+systemctl disable ngrok.service
+rm /lib/systemd/system/ngrok.service
+rm -rf /opt/ngrok
