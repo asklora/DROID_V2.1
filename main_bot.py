bb205ddc4f104da1dffea69a5bb0081efc46db30
Agustian <agustian@loratechai.com>
2021-03-08 08:52:32 +0700

    Classic

--- main_bot.py
+++ main_bot.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     # data_prep_history()
     # train_model()
     # infer_history()
-    option_maker_history_classic(ticker=None, currency_code=None, time_to_exp=None, mod=False, option_maker=False, null_filler=False)
+    option_maker_history_classic(option_maker=True, null_filler=False)
