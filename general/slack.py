df011b5e4f9342d1442fb76992f7c41fc798d862
Agustian <agustian@loratechai.com>
2021-03-10 15:10:51 +0700

    Ticker Symbol

--- general/slack.py
+++ general/slack.py
@@ -1,10 +1,10 @@
 import slack
-import os
+from global_vars import SLACK_CHANNEL, SLACK_API
 
-def report_to_slack(message, channel=os.getenv("SLACK_CHANNEL")):
+def report_to_slack(message, channel=SLACK_CHANNEL):
     print(channel)
     # try:
-    #     client = slack.WebClient(token=os.getenv("SLACK_API"), timeout=30)
+    #     client = slack.WebClient(token=SLACK_API, timeout=30)
     #     client.chat_postMessage(
     #         channel=channel,
     #         text=message)
