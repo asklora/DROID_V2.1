a6e611d20d229c8841cc9887e961fefec66fe1ff
jefri-irfej <jefri@loratechai.com>
2021-03-19 10:30:12 +0700

    dockerfile directory

--- installer/python_dependency.sh
+++ installer/python_dependency.sh
@@ -1,20 +1,19 @@
 echo "=========================="
 echo $PWD
-ls --all
 echo "=========================="
 if [ -f /var/lib/jenkins ];
 then 
 	echo "Updating Bastion instance ... "
-	pip3 install -r requirements_no_AI.txt
+	pip3 install -r installer/requirements_no_AI.txt
 else
 	if [ -f ~/Documents/droidDev ];
 	then
 		echo "Updating PC instance ... "
-		pip3 install -r requirements_AI.txt
+		pip3 install -r installer/requirements_AI.txt
 		pip3 uninstall numpy -y
 		pip3 install numpy
 	else
 		echo "Updating without AI ..."
-		pip3 install -r requirements_no_AI.txt
+		pip3 install -r installer/requirements_no_AI.txt
 	fi
 fi
