2211ecc5e7b0a1969135d2e5a9d4666a07732048
Agustian <agustian@loratechai.com>
2021-03-31 16:27:55 +0700

    Add requirement

--- test.py
+++ test.py
@@ -1,24 +1,19 @@
+from bot.data_download import get_latest_price
 import json
+import pandas as pd
 from pymongo import MongoClient
-from django.conf import settings
-from django.core.cache import cache
 
 
-mongo_url = 'mongodb+srv://postgres:postgres@cluster0.b0com.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+mongo_url = "mongodb+srv://postgres:postgres@cluster0.b0com.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client =  MongoClient(mongo_url)
 
 if __name__ == "__main__":
-    univ = DroidUniverse.objects.filter(is_active=True)[:3]
-    bot = BotOptionTypes.objects.all()
-    for instance in univ:
-        for attr in bot:
-            query = BotBacktest.objects.filter(
-                            ticker=instance.ticker, 
-                            month_to_exp=attr.bot_horizon_month, 
-                            bot_type=attr.bot_group, 
-                            option_type=attr.bot_option_type).order_by('ticker', 'spot_date')
-            value = BotBacktestSerializer(query,many=True).data
-            key = f'{instance.ticker}:{attr.bot_horizon_month}:{attr.bot_group}:{attr.bot_option_type}'
-            value = json.dumps(value)
-            cache.set(key, value)
-            print(key)
+    # Connect to MongoDB
+    db = client["latest_data"]
+    collection = db["latest_price"]
+
+    ticker = ["MSFT.O", "AAPL.O"]
+    data = get_latest_price(ticker=ticker)
+    data_dict = data.to_dict("records")
+    # Insert collection
+    collection.insert_many(data_dict)
