d259c11901a83a24b43581c4198353656e876701
redloratech <rede.akbar@loratechai.com>
2021-04-12 16:41:07 +0700

    add order

--- installer/requirement.sh
+++ installer/requirement.sh
@@ -20,4 +20,8 @@ if [ -x "$(command -v aws)" ]; then
 else
 	echo 'Error: aws is not installed.' >&2
 	echo 'Error: aws credentials not installed' >&2
+	apt-get install -yq awscli
+	aws configure set aws_access_key_id AKIA2XEOTUNGWEQ43TB6
+	aws configure set aws_secret_access_key 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
+	aws configure set default.region ap-east-1
 fi
