476e3fb76191f3b7c5285171019453e7ed7ddf56
redloratech <rede.akbar@loratechai.com>
2021-04-14 13:08:07 +0700

    change add order

--- core/djangomodule/crudlib/order.py
+++ core/djangomodule/crudlib/order.py
@@ -5,9 +5,16 @@ def sync_order(payload):
     try:
         order = Order.objects.get(uid=payload['uid'])
         for attrib, val in payload.items():
+            field= order._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                attrib = f'{attrib}_id'
             setattr(order, attrib,val)
         order.save()
     except Order.DoesNotExist:
+        for attrib, val in payload.items():
+            field= Order._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                payload[f'{attrib}_id'] = payload.pop(attrib)
         order = Order.objects.create(**payload)
     
     print(order)
@@ -16,20 +23,34 @@ def sync_postion(payload):
     try:
         position = OrderPosition.objects.get(uid=payload['uid'])
         for attrib, val in payload.items():
+            field= position._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                attrib = f'{attrib}_id'
             setattr(position, attrib,val)
         position.save()
     except OrderPosition.DoesNotExist:
+        for attrib, val in payload.items():
+            field= OrderPosition._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                payload[f'{attrib}_id'] = payload.pop(attrib)
         position = OrderPosition.objects.create(**payload)
     
     print(position)
 
 def sync_perfromance(payload):
     try:
-        performance = PositionPerformance.objects.get(uid=payload['uid'])
+        performance = PositionPerformance.objects.get(uid=payload['id'])
         for attrib, val in payload.items():
+            field= performance._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                attrib = f'{attrib}_id'
             setattr(performance, attrib,val)
         performance.save()
     except PositionPerformance.DoesNotExist:
+        for attrib, val in payload.items():
+            field= PositionPerformance._meta.get_field(attrib)
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                payload[f'{attrib}_id'] = payload.pop(attrib)
         performance = PositionPerformance.objects.create(**payload)
     
     print(performance)
--- core/djangomodule/management/commands/modulelib.py
+++ core/djangomodule/management/commands/modulelib.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand
 from core.services import *
+from core.orders.models import Order
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        data = {
-            'task':'hellow'
-        }
-        celery_app.send_task('config.celery.listener',args=(data,),queue='batch')
+        # data = {
+        #     'task':'hellow'
+        # }
+        # celery_app.send_task('config.celery.listener',args=(data,),queue='batch')
+        order = Order.objects.get(uid='8224d7d1-0f04-4429-83cc-002a0461dc2c')
+        fields= Order._meta.get_fields()
+        for field in fields:
+            if field.one_to_many or field.many_to_many or field.many_to_one or field.one_to_one:
+                print(field.name)
