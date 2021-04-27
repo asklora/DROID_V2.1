be6ce52e2a583f0893e007acd3ccfe538a90c48d
redloratech <rede.akbar@loratechai.com>
2021-04-27 16:49:06 +0700

    user asset ecr

--- core/djangomodule/management/commands/hedge.py
+++ core/djangomodule/management/commands/hedge.py
@@ -1,42 +1,37 @@
 from datetime import datetime
-from portfolio_hedge import test
 from django.core.management.base import BaseCommand
-from core.services import *
-from core.orders.models import Order,OrderPosition,PositionPerformance
-from core.master.models import LatestPrice
-from bot.calculate_bot import get_ucdc_detail,get_uno_detail,get_classic
+from portfolio.daily_hedge_classic import classic_position_check
+from portfolio.daily_hedge_ucdc import ucdc_position_check
+from portfolio.daily_hedge_uno import uno_position_check
+from core.orders.models import OrderPosition
 
 
 class Command(BaseCommand):
-    
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            '-c', '--celery', action='store_true', help='for celery')
+
     def handle(self, *args, **options):
-        test()
-        # orders = OrderPosition.objects.filter(is_live=True)
-        # for order in orders:
-        #     bot = order.bot
-        #     price = LatestPrice.objects.get(ticker=order.symbol)
-        #     today = datetime.now().date()
-        #     intraday_price = (price.intraday_ask + price.intraday_bid) * 0.5
-        #     try:
-        #         prev_performance = PositionPerformance.objects.filter(position=order).latest('created')
-        #     except PositionPerformance.DoesNotExist:
-        #         prev_performance = False
-        #     # create error log here
-        #     if prev_performance:
-        #         if bot.is_uno():
-        #             detail = get_uno_detail(order.symbol.ticker,order.symbol.currency_code,
-        #                         order.expiry,today,
-        #                             bot.time_to_exp,
-        #                         intraday_price,bot.bot_option_type,bot.bot_type.bot_type)
-        #             bot_cash_balance = prev_performance.current_bot_cash_balance + ((prev_performance.share_num - order.share_num) * intraday_price)
-        #             current_pnl_amt = prev_performance.current_pnl_amt + (intraday_price - prev_performance.last_live_price) * prev_performance.share_num
-        #         elif order.bot.is_ucdc():
-        #             detail = get_ucdc_detail(order.symbol.ticker,order.symbol.currency_code,
-        #                                      order.expiry,today,bot.time_to_exp,intraday_price,
-        #                                      bot.bot_option_type,bot.bot_type.bot_type)
-        #         elif order.bot.is_classic():
-        #             detail =''
-        #             pass
-                
-        #         print(detail)
-            
+        positions = OrderPosition.objects.filter(is_live=True)
+        for position in positions:
+            position_uid = position.position_uid
+            if (position.bot.is_uno()):
+                if options['celery']:
+                    status = uno_position_check.apply_async(
+                        args=(position_uid,), queue='droid')
+                else:
+                    status = uno_position_check(position_uid)
+            elif (position.bot.is_ucdc()):
+                if options['celery']:
+                    status = ucdc_position_check.apply_async(
+                        args=(position_uid,), queue='droid')
+                else:
+                    status = ucdc_position_check(position_uid)
+            elif (position.bot.is_classic()):
+                if options['celery']:
+                    status = classic_position_check.apply_async(
+                        args=(position_uid,), queue='droid')
+                else:
+                    status = classic_position_check(position_uid)
+            print(status)
