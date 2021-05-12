60244cfb256a5139135990421d5fcaa8eda400f5
Agustian <agustian@loratechai.com>
2021-05-12 10:30:03 +0700

    New Schedule For Uno UCDC

---
+++ core/djangomodule/management/commands/backtest.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from main_executive import daily_classic, daily_shcedule_uno_ucdc
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("-run_number", "--run_number", help="run_number", type=int, default=0)
+        parser.add_argument("-total_no_of_runs", "--total_no_of_runs", help="total_no_of_runs", type=int, default=1)
+        parser.add_argument("-time_to_exp", "--time_to_exp", type=str, help="time_to_exp",)
+        parser.add_argument("-currency_code", "--currency_code", type=str, help="currency_code")
+        parser.add_argument("-uno", "--uno", type=bool, help="uno", default=False)
+        parser.add_argument("-ucdc", "--ucdc", type=bool, help="ucdc", default=False)
+        parser.add_argument("-classic", "--classic", type=bool, help="classic", default=False)
+        parser.add_argument("-infer", "--infer", type=bool, help="infer", default=False)
+        parser.add_argument("-option_maker", "--option_maker", type=bool, help="option_maker", default=False)
+        parser.add_argument("-null_filler", "--null_filler", type=bool, help="null_filler", default=False)
+        parser.add_argument("-statistic", "--statistic", type=bool, help="statistic", default=False)
+        parser.add_argument("-prep", "--prep", type=bool, help="prep", default=False)
+        parser.add_argument("-latest_data", "--latest_data", type=bool, help="latest_data", default=False)
+        parser.add_argument("-ranking", "--ranking", type=bool, help="ranking", default=False)
+        parser.add_argument("-backtest", "--backtest", type=bool, help="backtest", default=False)
+    def handle(self, *args, **options):
+        if(options["classic"]):
+            daily_classic()
+        else:
+            daily_shcedule_uno_ucdc(currency_code=[options["currency_code"]], uno=options["uno"], 
+            ucdc=options["ucdc"], infer=options["infer"], option_maker=options["option_maker"], 
+            null_filler=options["null_filler"], statistic=options["statistic"], prep=options["prep"], 
+            latest_data=options["latest_data"], ranking=options["ranking"], backtest=options["backtest"])
--- main_executive.py
+++ main_executive.py
@@ -26,6 +26,40 @@ def training(ticker=None, currency_code=None):
     train_lebeler_model(ticker=ticker, currency_code=currency_code)
 
 # follow currency schedule
+def daily_shcedule_uno_ucdc(ticker=None, currency_code=None, time_to_exp=time_to_expiry, option_maker=True, null_filler=True, mod=False, total_no_of_runs=1, run_number=0, uno=False, ucdc=False, prep=False, statistic=False, infer=True, latest_data=False, ranking=False, backtest=False):
+    #Data Preparation
+    if(prep):
+        data_prep_daily(ticker=ticker, currency_code=currency_code)
+        data_prep_check_new_ticker(ticker=ticker, currency_code=currency_code)
+        do_function("latest_bot_data")
+    #Populate Volatility Infer
+    if(infer):
+        infer_daily(ticker=ticker, currency_code=currency_code)
+        do_function("latest_vol")
+    if(latest_data):
+        #Latest Bot Update Populate
+        populate_latest_bot_update(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp)
+    if(uno):
+        #Option Maker & Null Filler UNO
+        option_maker_uno_check_new_ticker(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp, mod=mod, option_maker=option_maker, null_filler=null_filler, infer=infer, total_no_of_runs=total_no_of_runs, run_number=run_number)
+        option_maker_daily_uno(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp, mod=mod, option_maker=option_maker, null_filler=null_filler, infer=infer, total_no_of_runs=total_no_of_runs, run_number=run_number)
+    if(ucdc):
+        #Option Maker & Null Filler UCDC
+        option_maker_ucdc_check_new_ticker(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp, mod=mod, option_maker=option_maker, null_filler=null_filler, infer=infer, total_no_of_runs=total_no_of_runs, run_number=run_number)
+        option_maker_daily_ucdc(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp, mod=mod, option_maker=option_maker, null_filler=null_filler, infer=infer, total_no_of_runs=total_no_of_runs, run_number=run_number)
+    if(backtest):
+        #Update Bot Backtest
+        do_function("bot_backtest_updates")
+    if(ranking):
+        #Populate Bot Ranking
+        bot_ranking_daily(ticker=ticker, currency_code=currency_code, mod=mod)
+        #Update Bot Ranking to Latest Bot Update
+        do_function("latest_bot_update")
+    if(statistic):
+        bot_statistic_ucdc(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp)
+        bot_statistic_uno(ticker=ticker, currency_code=currency_code, time_to_exp=time_to_exp)
+
+# follow currency schedule
 def daily_uno_ucdc(ticker=None, currency_code=None, time_to_exp=time_to_expiry, infer=True, option_maker=True, null_filler=True, mod=False, total_no_of_runs=1, run_number=0):
     #Data Preparation
     data_prep_daily(ticker=ticker, currency_code=currency_code)
