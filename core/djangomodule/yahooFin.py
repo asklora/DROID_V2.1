65c341d312b508bd2cbc69792cba3a285cfa78ac
= <=>
2021-05-17 20:48:31 +0700

    add yahoo fin

--- core/djangomodule/yahooFin.py
+++ core/djangomodule/yahooFin.py
@@ -2,18 +2,56 @@ import pandas as pd
 import requests
 import json
 import ast
-from core.djangomodule.network.cloud import DroidDb
-# from core.universe.models import Universe
+# from core.djangomodule.network.cloud import DroidDb
+from core.universe.models import Universe
 
-def yahoo(ticker):
+def get_quote_yahoo(ticker, use_symbol=False):
 	api_key = "48c15ceb22mshe6bb12d6f379d74p146379jsnffadab4cee19"
 	url = "https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote"
 	header = {
 		'x-rapidapi-key': api_key,
 		'x-rapidapi-host': "yahoo-finance-low-latency.p.rapidapi.com"
 	}
+
 	if isinstance(ticker, list):
-		ticker = ','.join([str(elem) for elem in ticker])
+		if use_symbol:
+			ticker_ric = []
+			for tick in ticker:
+				try:
+					ric = Universe.objects.get(ticker_symbol=tick)
+					if ric.currency_code.currency_code != 'USD':
+						ticker_ric.append(ric.ticker)
+					else:
+						ticker_ric.append(tick)
+				except Universe.DoesNotExist:
+					pass
+			ticker = ticker_ric
+		if ticker != []:
+			ticker = ','.join([str(elem) for elem in ticker])
+		else:
+			data = {
+					"ticker":[],
+					"ask":[],
+					"bid":[]
+				}
+			return pd.DataFrame(data)
+	else:
+		try:
+			ric = Universe.objects.get(ticker_symbol=ticker)
+			if ric.currency_code.currency_code == 'USD':
+				ticker = ric.ticker_symbol
+			else:
+				if use_symbol:
+					ticker = ric.ticker
+				else:
+					ticker = ticker
+		except Universe.DoesNotExist:
+			data = {
+					"ticker":[],
+					"ask":[],
+					"bid":[]
+				}
+			return pd.DataFrame(data)
 
 	symbol = {
 		"symbols" : ticker
