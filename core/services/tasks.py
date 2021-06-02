8bb58c4db0852d68f37dbe86e373e78b8d092f38
redloratech <rede.akbar@loratechai.com>
2021-06-02 12:03:05 +0700

    fix api [ci deploy] ecr

--- core/services/tasks.py
+++ core/services/tasks.py
@@ -332,6 +332,6 @@ def send_csv_hanwha(currency=None, client_name=None, new=None):
                             hanwha_csv, mimetype="text/csv")
         draft_email.attach(f"{currency}_{now}_asklora.csv",
                            csv, mimetype="text/csv")
-        draft_email.send()
-        hanwha_email.send()
+        # draft_email.send()
+        # hanwha_email.send()
     return {'result': f'send email {currency} done'}
