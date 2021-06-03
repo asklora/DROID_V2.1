7f91b3402132b5ad352e08a4a08090eb3ddbcfa9
redloratech <rede.akbar@loratechai.com>
2021-06-03 12:01:40 +0700

    modify weekly top pick 2

--- client_test_pick.py
+++ client_test_pick.py
@@ -293,10 +293,19 @@ def populate_bot_advisor(currency_code=None, client_name="HANWHA", top_pick_stoc
             uid = f"{client_uid}_{currency_code[0]}_{ticker}_{spot_date}_{service_type}_{capital}"
             expiry_date = get_expiry_date(time_to_exp[0], str(spot_date), currency_code[0])
             uid = uid.replace("-", "").replace(".", "").replace(" ", "")
+            #### ADD INTERVAL WEEK OF YEAR ####
+            dates = pd.to_datetime(spot_date)
+            week = dates.isocalendar()[1]
+            year = dates.isocalendar()[0]
+            interval = f'{year}{week}'
+            ##### ================ #####
             temp = pd.DataFrame({"created":[spot_date], "updated":[spot_date], "uid":[uid],"spot_date":[spot_date], "bot":[None], 
             "expiry_date":[expiry_date], "has_position":["False"], "position_uid":[None],"execution_date":[None], "completed_date":[None], "event":[None],
             "rank":[count], "client_uid":[client_uid], "ticker":[ticker],"bot_id":[bot_id], "currency_code":[currency_code[0]], "service_type":[service_type], 
-            "capital":[capital], "status":["Available"]}, index=[0])
+            "capital":[capital], 
+            "status":["Pending"], # CHANGE TO PENDING DEFAULT
+            "week_of_year":[int(interval)] # ADD INTERVAL TO DATAFRAME
+            }, index=[0])
             last_ticker.append(ticker)
             count+=1
             advisor_pick = advisor_pick.append(temp)
