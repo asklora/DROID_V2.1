2029a3c8b2cfa5c3d075f51f964fc6b151a15c14
redloratech <rede.akbar@loratechai.com>
2021-06-07 11:49:08 +0700

    update classic vol dev-run

--- core/djangomodule/management/commands/debug.py
+++ core/djangomodule/management/commands/debug.py
@@ -72,6 +72,6 @@ class Command(BaseCommand):
         # print(user.client_user.all()[0].client.client_uid)
         # migrate_droid1.apply_async(queue='droid')
         # print(daily_hedge(currency="KRW"))
-        send_csv_hanwha(currency="KRW")
+        # send_csv_hanwha(currency="KRW")
         # send_csv_hanwha(currency="CNY")
         # send_csv_hanwha(currency="HKD")
