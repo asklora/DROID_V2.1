67cfb437c5a14fd85bb2a21c8b69379de9a60650
redloratech <rede.akbar@loratechai.com>
2021-06-21 15:21:49 +0700

    create send email command [ci deploy] ecr

--- core/djangomodule/management/commands/send_hedge_email.py
+++ core/djangomodule/management/commands/send_hedge_email.py
@@ -1,4 +1,10 @@
 from core.services.tasks import send_csv_hanwha
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("-currency", "--currency", type=str, help="currency")
+    
+    
+    
     def handle(self, *args, **options):
+        send_csv_hanwha(currency=options['currency'])
