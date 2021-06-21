e982cf87289d8df1132a81e85bc600f30c457895
redloratech <rede.akbar@loratechai.com>
2021-06-21 14:39:49 +0700

    fix hedge shares [ci deploy] ecr

--- core/djangomodule/management/commands/debug.py
+++ core/djangomodule/management/commands/debug.py
@@ -15,7 +15,7 @@ import traceback as trace
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        scrap_csi()
+        # scrap_csi()
 
         # user = User.objects.get(id=1)
         # print(user.check_password('pbkdf2_sha256$216000$SOyf9SnnXmzC$tpeNQM5F/AFhMMJNFnkZz='))
