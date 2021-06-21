72995f27b10d67c0a152bfb50992d2ca12f5ffdd
Agustian <agustian@loratechai.com>
2021-06-21 10:48:48 +0700

    dev-run

--- general/slack.py
+++ general/slack.py
@@ -4,18 +4,17 @@ from dotenv import load_dotenv
 from environs import Env
 
 def report_to_slack(message, channel=SLACK_CHANNEL):
-    print(message)
-    # try:
-    #     env = Env()
-    #     load_dotenv()
-    #     debug=env.bool("DROID_DEBUG")
-    #     if debug:
-    #         message = "[TEST DB] " + message
+    try:
+        env = Env()
+        load_dotenv()
+        debug=env.bool("DROID_DEBUG")
+        if debug:
+            message = "[TEST DB] " + message
 
-    #     client = slack.WebClient(token=SLACK_API, timeout=30)
-    #     client.chat_postMessage(
-    #         channel=channel,
-    #         text=message)
-    # except Exception as e:
-    #     print(e)
+        client = slack.WebClient(token=SLACK_API, timeout=30)
+        client.chat_postMessage(
+            channel=channel,
+            text=message)
+    except Exception as e:
+        print(e)
     
