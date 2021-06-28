1930968b4075395764b25b39c943af45f4485010
redloratech <rede.akbar@loratechai.com>
2021-06-28 16:07:27 +0700

    B2B-280 push to dev dev-run

--- core/user/urls.py
+++ core/user/urls.py
@@ -5,5 +5,4 @@ from .views import UserProfile,UserSummaryView
 urlpatterns = [
     path('me/', UserProfile.as_view(), name='profile'),
     path('summary/<int:pk>/', UserSummaryView.as_view(), name='summary'),
-
 ]
