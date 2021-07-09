791c6b9f0acf93700c4647dd547eab646e350150
redloratech <rede.akbar@loratechai.com>
2021-07-09 09:00:54 +0700

    SKLR-146 SKLR-147 SKLR-148 SKLR-149 SKLR-150 #comment run in dev dev-run

--- core/universe/consumer.py
+++ core/universe/consumer.py
@@ -1,10 +1,9 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
-from datasource import rkd
+from asgiref.sync import async_to_sync,sync_to_async
+from channels.generic.websocket import WebsocketConsumer,AsyncWebsocketConsumer
+from datasource.rkd import RkdStream
 from .models import Universe
-
-
+import asyncio
 class UniverseConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['subscribe']
@@ -44,3 +43,66 @@ class UniverseConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': event
         }))
+
+
+
+class DurableConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        type = text_data_json['type']
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': type,
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+    
+    def tell(self):
+        return sync_to_async(RkdStream)()
+    async def streaming(self,event):
+        print('sss',event)
+        rkd =  await self.tell()
+        rkd.ticker_data = event['message']
+        result = await sync_to_async(rkd.stream())
+        print(result)
+         # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': 'stopped'
+            }
+        )
--- datasource/rkd.py
+++ datasource/rkd.py
@@ -369,7 +369,7 @@ class RkdData(Rkd):
 
 class RkdStream(RkdData):
     ID =[]
-
+    chanels = None
     def __init__(self, *args, **kwargs):
         self.kwargs = kwargs
         super().__init__(*args, **kwargs)
@@ -503,9 +503,9 @@ class RkdStream(RkdData):
             df = pd.DataFrame(data).rename(columns=change)
             ticker = df.loc[df['ticker'] == message['Fields']['ticker']]
             print(df)
-            # asyncio.run(self.layer.group_send(message['Fields']['ticker'],
+            # asyncio.run(self.layer.group_send(self.chanels,
             #                                   {
-            #     'type': 'broadcastmessage',
+            #     'type': 'chat_message',
             #     'message':  ticker.to_dict('records')
             # }))
             # asyncio.run(self.layer.group_send('topstock',
