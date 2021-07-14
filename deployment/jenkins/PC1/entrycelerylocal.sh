bd23bdda43dcc42fc5c998b95c3c06c1d80e9ee4
redloratech <rede.akbar@loratechai.com>
2021-07-14 20:07:56 +0700

    config deployment

--- deployment/jenkins/PC1/entrycelerylocal.sh
+++ deployment/jenkins/PC1/entrycelerylocal.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 celery -A core.services worker -l INFO --hostname=PC1@%h -Q PC1
--- deployment/jenkins/bastion/devcelery.sh
+++ deployment/jenkins/bastion/devcelery.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 export DJANGO_SETTINGS_MODULE=config.development
 celery -A core.services worker --beat -l  INFO --hostname=master@%h --concurrency=12 --scheduler django_celery_beat.schedulers:DatabaseScheduler
