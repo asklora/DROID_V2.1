2501158db390178533062c215ad146c1628a9cfd
Hendika Nugroho <hendika.nugroho@loratechai.com>
2021-08-10 13:52:22 +0700

    add docstrings to some models, format the file and reorder modules

--- core/djangomodule/models.py
+++ core/djangomodule/models.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 
 class BaseTimeStampModel(models.Model):
+    """
+    Base model for timestamp support, related models: :model:`Clients.Client` and its related models, :model:`orders.Order` etc.
+    """
     created = models.DateTimeField(editable=True)
     updated = models.DateTimeField(editable=False)
 
