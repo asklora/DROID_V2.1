36afd035e3acbc660b85e60c0b829e98b42c808b
redloratech <rede.akbar@loratechai.com>
2021-08-17 12:07:19 +0700

    signal handler

--- core/orders/order_signal_action.py
+++ core/orders/order_signal_action.py
@@ -3,10 +3,21 @@ from core.user.models import TransactionHistory
 from .models import Order, OrderFee, OrderPosition, PositionPerformance
 from abc import ABC,abstractmethod
 
+"""
+user/bot
+broker/simulation
+init/hedge
+    hedge expired,finish / hedge daily
 
+buy/sell
 
-class OrderActorActionBase(ABC):
+"""
+
+class AbstracBaseOrder(ABC):
     
+    @abstractmethod
+    def on_placed(self):
+        pass
     
     @abstractmethod
     def on_pending(self):
@@ -20,6 +31,17 @@ class OrderActorActionBase(ABC):
     @abstractmethod
     def on_cancel(self):
         pass
+    
+    
+
+
+class OrderActorActionBase(AbstracBaseOrder):
+    
+    
+    
+    def __init__(self,*args,**kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
     
     def begin_action(self):
         if self.instance.status == 'pending':
@@ -29,70 +51,29 @@ class OrderActorActionBase(ABC):
         elif self.instance.status == 'cancel':
             self.on_cancel()
 
-class BotOrder(OrderActorActionBase):
+class SimulationOrder(OrderActorActionBase):
 
-    def __init__(self,*args,**kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
+    def on_placed(self):
+        pass
     
-
-
-
     def on_pending(self):
         if self.instance.is_init:
+            """
+            take user balance into Order
+            """
             inv_amt = self.instance.setup['investment_amount']
-        digits = max(min(5-len(str(int(self.instance.price))), 2), -1)
-        TransactionHistory.objects.create(
-            balance_uid=self.instance.user_id.wallet,
-            side="debit",
-            amount=round(inv_amt, digits),
-            transaction_detail={
-                "last_amount" : self.user_wallet_amount,
-                "description": "bot order",
-                "event": "create",
-                "order_uid": str(self.instance.order_uid)
-            },
-        )
-        # if not instance.order_type:
-        #     commissions_fee, stamp_duty_fee, total_fee = calculate_fee(
-        #         instance.amount, instance.side, instance.user_id)
-        #     fee = OrderFee.objects.create(
-        #         order_uid=instance,
-        #         fee_type=f"{instance.side} commissions fee",
-        #         amount=commissions_fee
-        #     )
-        #     balance = Accountbalance.objects.get(user_id=instance.user_id)
-        #     TransactionHistory.objects.create(
-        #         balance_uid=instance.user_id.wallet,
-        #         side="debit",
-        #         amount=total_fee,
-        #         transaction_detail={
-        #             "description": f"{instance.side} fee",
-        #             # "position": f"{order.position_uid}",
-        #             "event": "fee",
-        #             "fee_id": fee.id
-        #         },
-        #     )
-        #     if stamp_duty_fee > 0:
-        #         stamp = OrderFee.objects.create(
-        #             order_uid=instance,
-        #             fee_type=f"{instance.side} stamp_duty fee",
-        #             amount=stamp_duty_fee
-        #         )
-        #         balance = Accountbalance.objects.get(user_id=instance.user_id)
-        #         TransactionHistory.objects.create(
-        #             balance_uid=instance.user_id.wallet,
-        #             side="debit",
-        #             amount=stamp_duty_fee,
-        #             transaction_detail={
-        #                 "last_amount" : balance.amount,
-        #                 "description": f"{instance.side} fee",
-        #                 # "position": f"{order.position_uid}",
-        #                 "event": "stamp_duty",
-        #                 "stamp_duty_id": stamp.id
-        #             },
-        #         )
+            digits = max(min(5-len(str(int(self.instance.price))), 2), -1)
+            TransactionHistory.objects.create(
+                balance_uid=self.instance.user_id.wallet,
+                side="debit",
+                amount=round(inv_amt, digits),
+                transaction_detail={
+                    "last_amount" : self.user_wallet_amount,
+                    "description": "bot order",
+                    "event": "create",
+                    "order_uid": str(self.instance.order_uid)
+                },
+            )
 
 
     def on_filled(self):
@@ -103,13 +84,14 @@ class BotOrder(OrderActorActionBase):
     
 
 
-class UserOrder(OrderActorActionBase):
+class LiveOrder(OrderActorActionBase):
 
     def __init__(self,*args,**kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-
+    def on_placed(self):
+        pass
     
     def on_pending(self):
         pass
@@ -136,48 +118,10 @@ class OrderServices:
         self.user_handler = UserOrder(**order_property)
 
     
-
-    def bot_hedge(self):
-        pass
-
     
-    def calculate_fee(self,amount, side, user_id):
-        pass
-    
-    
-    def create_fee(self, user_id, balance_uid, position_uid, status):
-        pass
-
-    def common_order_method(self):
-        pass
-
-    def execute_sell(self):
-        pass
-
-    def execute_buy(self):
-        pass
-    
-    def init_order_method(self):
+    def process_transaction(self):
         if self.bot.is_stock():
-            self.init_user_order()
+            self.user_handler
         else:
-            self.init_bot_order()
+            self.bot_handler
     
-    def init_bot_order(self):
-        pass
-    
-    def init_user_order(self):
-        if self.instance.status == 'pending':
-            self.on_pending()
-        elif self.instance.status == 'filled':
-            self.on_filled()
-        elif self.instance.status == 'cancel':
-            self.on_cancel()
-
-    
-   
-    def order_identity(self):
-        if self.instance.init:
-            self.init_order_method()
-        else:
-            self.common_order_method()
