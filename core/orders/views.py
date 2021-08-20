db1556fd2c40e9193e6264b1bdf257d9d407550b
Hendika Nugroho <hendika.nugroho@loratechai.com>
2021-08-20 13:10:42 +0700

    format views.py

--- core/orders/views.py
+++ core/orders/views.py
@@ -1,3 +1,13 @@
+from core.djangomodule.general import IsRegisteredUser, errserializer
+from rest_framework import viewsets, views, response, status
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiResponse,
+    OpenApiParameter,
+)
+from core.Clients.models import UserClient
+from .models import OrderPosition, PositionPerformance, Order
 from .serializers import (
     PositionSerializer,
     PerformanceSerializer,
@@ -6,24 +16,47 @@ from .serializers import (
     OrderDetailsSerializers,
     OrderListSerializers,
     OrderActionSerializer,
-    OrderPortfolioCheckSerializer
+    OrderPortfolioCheckSerializer,
 )
-from rest_framework import viewsets, views, response, status, serializers
-from rest_framework.decorators import action
-from .models import OrderPosition, PositionPerformance, Order
-from core.Clients.models import UserClient
-from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiResponse, OpenApiParameter, OpenApiTypes
-from core.djangomodule.general import IsRegisteredUser,errserializer
 
 
+class BotPerformanceViews(views.APIView):
+    """
+    get bot Performance by positions
+    """
 
+    serializer_class = PerformanceSerializer
+    permission_classes = (IsRegisteredUser,)
 
-
-@extend_schema_view(
-    list=extend_schema(
-        operation_id='Get positions by Client'
+    @extend_schema(
+        operation_id="Get bot Performance by positions",
+        responses={
+            200: OpenApiResponse(
+                response=PerformanceSerializer,
+            ),
+            404: OpenApiResponse(
+                description="Bad request (position not found)", response=errserializer
+            ),
+            401: OpenApiResponse(
+                description="Unauthorized request", response=errserializer
+            ),
+        },
     )
-)
+    def get(self, request, position_uid):
+        perf = PositionPerformance.objects.filter(position_uid=position_uid).order_by(
+            "created"
+        )
+        if not perf.exists():
+            return response.Response(
+                {"message": f"{position_uid} doesnt exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        return response.Response(
+            PerformanceSerializer(perf, many=True).data, status=status.HTTP_200_OK
+        )
+
+
+@extend_schema_view(list=extend_schema(operation_id="Get positions by Client"))
 class PositionViews(viewsets.ReadOnlyModelViewSet):
     """
     get positions by Client
@@ -34,23 +67,29 @@ class PositionViews(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsRegisteredUser,)
 
     def get_queryset(self):
-        if self.request.user.email == 'asklora@loratechai.com':
+        if self.request.user.email == "asklora@loratechai.com":
             return self.queryset
 
         user = self.request.user
         client = user.client_user.all().first()
-        user_id = [user.user_id for user in UserClient.objects.filter(
-            client=client.client)]
+        user_id = [
+            user.user_id for user in UserClient.objects.filter(client=client.client)
+        ]
 
         return OrderPosition.objects.filter(user_id__in=user_id)
 
 
 @extend_schema_view(
     list=extend_schema(
-        operation_id='Get positions by Account',
+        operation_id="Get positions by Account",
         parameters=[
-            OpenApiParameter(name='live',required=False,type=int,description='1 is true (still live), 0 is false (completed)')
-        ]
+            OpenApiParameter(
+                name="live",
+                required=False,
+                type=int,
+                description="1 is true (still live), 0 is false (completed)",
+            )
+        ],
     )
 )
 class PositionUserViews(viewsets.ReadOnlyModelViewSet):
@@ -62,75 +101,76 @@ class PositionUserViews(viewsets.ReadOnlyModelViewSet):
     - complete positions
         - url/?complete
     """
+
     serializer_class = PositionSerializer
     queryset = OrderPosition.objects.all()
     permission_classes = (IsRegisteredUser,)
 
     def get_queryset(self):
         if self.kwargs:
-            if 'live' in self.request.query_params:
-                params = self.request.query_params.get('live',None)
-                if params not in ['0','1']:
+            if "live" in self.request.query_params:
+                params = self.request.query_params.get("live", None)
+                if params not in ["0", "1"]:
                     return []
                 if params:
-                    return OrderPosition.objects.prefetch_related('ticker').filter(user_id=self.kwargs['user_id'], is_live=params)
+                    return OrderPosition.objects.prefetch_related("ticker").filter(
+                        user_id=self.kwargs["user_id"], is_live=params
+                    )
                 else:
-                    return OrderPosition.objects.prefetch_related('ticker').filter(user_id=self.kwargs['user_id'], is_live=True)
-            elif 'complete' in self.request.query_params:
-                return OrderPosition.objects.prefetch_related('ticker').filter(user_id=self.kwargs['user_id'], is_live=False)
-            return OrderPosition.objects.prefetch_related('ticker').filter(user_id=self.kwargs['user_id'])
+                    return OrderPosition.objects.prefetch_related("ticker").filter(
+                        user_id=self.kwargs["user_id"], is_live=True
+                    )
+            elif "complete" in self.request.query_params:
+                return OrderPosition.objects.prefetch_related("ticker").filter(
+                    user_id=self.kwargs["user_id"], is_live=False
+                )
+            return OrderPosition.objects.prefetch_related("ticker").filter(
+                user_id=self.kwargs["user_id"]
+            )
         else:
-            return OrderPosition.objects.prefetch_related('ticker').filter(user_id=None)
+            return OrderPosition.objects.prefetch_related("ticker").filter(user_id=None)
+
 
 class PositionDetailViews(views.APIView):
     """
     get Detail positions
     """
+
     serializer_class = PositionSerializer
     permission_classes = (IsRegisteredUser,)
 
     @extend_schema(
-        operation_id='Get bot Performance by positions',
+        operation_id="Get bot Performance by positions",
         responses={
-            200: OpenApiResponse(response=PositionSerializer,),
-            404: OpenApiResponse(description='Bad request (position not found)', response=errserializer),
-            401: OpenApiResponse(description='Unauthorized request', response=errserializer),
-        }
+            200: OpenApiResponse(
+                response=PositionSerializer,
+            ),
+            404: OpenApiResponse(
+                description="Bad request (position not found)", response=errserializer
+            ),
+            401: OpenApiResponse(
+                description="Unauthorized request", response=errserializer
+            ),
+        },
     )
     def get(self, request, position_uid):
         user = request.user
-        position = OrderPosition.objects.filter(
-            position_uid=position_uid)
+        position = OrderPosition.objects.filter(position_uid=position_uid)
         if position.exists():
             position = position.get()
             if position.user_id == user or user.is_superuser:
-                return response.Response(PositionSerializer(position).data, status=status.HTTP_200_OK)
+                return response.Response(
+                    PositionSerializer(position).data, status=status.HTTP_200_OK
+                )
             else:
-                return response.Response({'detail': f'this position not belong to current user'}, status=status.HTTP_403_FORBIDDEN)
+                return response.Response(
+                    {"detail": f"this position not belong to current user"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
 
-        return response.Response({'detail': f'{position_uid} doesnt exist'}, status=status.HTTP_404_NOT_FOUND)
-
-class BotPerformanceViews(views.APIView):
-    """
-    get bot Performance by positions
-    """
-    serializer_class = PerformanceSerializer
-    permission_classes = (IsRegisteredUser,)
-
-    @extend_schema(
-        operation_id='Get bot Performance by positions',
-        responses={
-            200: OpenApiResponse(response=PerformanceSerializer,),
-            404: OpenApiResponse(description='Bad request (position not found)', response=errserializer),
-            401: OpenApiResponse(description='Unauthorized request', response=errserializer),
-        }
-    )
-    def get(self, request, position_uid):
-        perf = PositionPerformance.objects.filter(
-            position_uid=position_uid).order_by('created')
-        if not perf.exists():
-            return response.Response({'message': f'{position_uid} doesnt exist'}, status=status.HTTP_404_NOT_FOUND)
-        return response.Response(PerformanceSerializer(perf, many=True).data, status=status.HTTP_200_OK)
+        return response.Response(
+            {"detail": f"{position_uid} doesnt exist"}, status=status.HTTP_404_NOT_FOUND
+        )
 
 
 class OrderViews(views.APIView):
@@ -138,44 +178,59 @@ class OrderViews(views.APIView):
     permission_classes = (IsRegisteredUser,)
 
     @extend_schema(
-        operation_id='Create new orders',
+        operation_id="Create new orders",
         responses={
             201: OpenApiResponse(response=OrderCreateSerializer),
-            404: OpenApiResponse(description='User not found', response=errserializer),
-            406: OpenApiResponse(description='Bad request (check your parameters)', response=errserializer),
-        }
+            404: OpenApiResponse(description="User not found", response=errserializer),
+            406: OpenApiResponse(
+                description="Bad request (check your parameters)",
+                response=errserializer,
+            ),
+        },
     )
-    def post(self, request):                
+    def post(self, request):
         serializer = OrderCreateSerializer(
-            data=request.data, context={'request': request})
+            data=request.data, context={"request": request}
+        )
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-        return response.Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class OrderPortfolioCheckView(views.APIView):
     """
     check user positions
     """
+
     serializer_class = OrderPortfolioCheckSerializer
     permission_classes = (IsRegisteredUser,)
+
     @extend_schema(
-        operation_id='check positions',
+        operation_id="check positions",
         responses={
-            200: OpenApiResponse(response=OrderPortfolioCheckSerializer,),
-            404: OpenApiResponse(description='Bad request (position not found)', response=errserializer),
-            401: OpenApiResponse(description='Unauthorized request', response=errserializer),
+            200: OpenApiResponse(
+                response=OrderPortfolioCheckSerializer,
+            ),
+            404: OpenApiResponse(
+                description="Bad request (position not found)", response=errserializer
+            ),
+            401: OpenApiResponse(
+                description="Unauthorized request", response=errserializer
+            ),
         },
-        description="check user positions"
+        description="check user positions",
     )
     def post(self, request):
 
         serializer = OrderPortfolioCheckSerializer(
-            data=request.data, context={'request': request})
+            data=request.data, context={"request": request}
+        )
         if serializer.is_valid():
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_200_OK)
         return response.Response(serializer.errors)
+
 
 class OrderUpdateViews(views.APIView):
     serializer_class = OrderUpdateSerializer
@@ -184,14 +239,22 @@ class OrderUpdateViews(views.APIView):
     def post(self, request):
         try:
             instance = OrderUpdateSerializer.Meta.model.objects.get(
-                order_uid=request.data['order_uid'])
+                order_uid=request.data["order_uid"]
+            )
         except OrderUpdateSerializer.Meta.model.DoesNotExist:
-            return response.Response({'detail': 'order not found'}, status=status.HTTP_404_NOT_FOUND)
+            return response.Response(
+                {"detail": "order not found"}, status=status.HTTP_404_NOT_FOUND
+            )
         # ignore if fels account
         if not instance.user_id.id == 135:
             if instance.user_id.username != request.user.username:
-                return response.Response({'detail': 'credentials not allowed to change this order'}, status=status.HTTP_403_FORBIDDEN)
-        serializer = OrderUpdateSerializer(instance, data=request.data,context={'request': request})
+                return response.Response(
+                    {"detail": "credentials not allowed to change this order"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+        serializer = OrderUpdateSerializer(
+            instance, data=request.data, context={"request": request}
+        )
         if serializer.is_valid():
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_200_OK)
@@ -199,7 +262,8 @@ class OrderUpdateViews(views.APIView):
 
 
 class OrderGetViews(viewsets.ViewSet):
-    permission_classes =(IsRegisteredUser,)
+    permission_classes = (IsRegisteredUser,)
+
     @extend_schema(
         responses=OrderListSerializers,
         # more customizations
@@ -207,8 +271,12 @@ class OrderGetViews(viewsets.ViewSet):
     def list(self, request):
         instances = Order.objects.filter(user_id=request.user)
         self.serialzer_class = OrderListSerializers
-        return response.Response(OrderListSerializers(instances, many=True).data, status=status.HTTP_200_OK)
-    permission_classes =(IsRegisteredUser,)
+        return response.Response(
+            OrderListSerializers(instances, many=True).data, status=status.HTTP_200_OK
+        )
+
+    permission_classes = (IsRegisteredUser,)
+
     @extend_schema(
         responses=OrderDetailsSerializers,
         # more customizations
@@ -217,9 +285,13 @@ class OrderGetViews(viewsets.ViewSet):
         try:
             instance = Order.objects.get(order_uid=order_uid)
             self.serialzer_class = OrderDetailsSerializers
-            return response.Response(OrderDetailsSerializers(instance).data, status=status.HTTP_200_OK)
+            return response.Response(
+                OrderDetailsSerializers(instance).data, status=status.HTTP_200_OK
+            )
         except Order.DoesNotExist:
-            return response.Response({'detail': 'order not forund'}, status=status.HTTP_404_NOT_FOUND)
+            return response.Response(
+                {"detail": "order not forund"}, status=status.HTTP_404_NOT_FOUND
+            )
 
 
 class OrderActionViews(views.APIView):
@@ -229,16 +301,24 @@ class OrderActionViews(views.APIView):
     def post(self, request):
         try:
             instance = OrderActionSerializer.Meta.model.objects.get(
-                order_uid=request.data['order_uid'])
+                order_uid=request.data["order_uid"]
+            )
         except OrderActionSerializer.Meta.model.DoesNotExist:
-            return response.Response({'detail': 'order not found'}, status=status.HTTP_404_NOT_FOUND)
+            return response.Response(
+                {"detail": "order not found"}, status=status.HTTP_404_NOT_FOUND
+            )
         except KeyError as e:
             err = str(e)
-            return response.Response({'detail': f'error key {err}'}, status=status.HTTP_400_BAD_REQUEST)
-       # ignore if fels account
+            return response.Response(
+                {"detail": f"error key {err}"}, status=status.HTTP_400_BAD_REQUEST
+            )
+        # ignore if fels account
         if not instance.user_id.id == 135:
             if instance.user_id.username != request.user.username:
-                return response.Response({'detail': 'credentials not allowed to change this order'}, status=status.HTTP_403_FORBIDDEN)
+                return response.Response(
+                    {"detail": "credentials not allowed to change this order"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
         serializer = OrderActionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
