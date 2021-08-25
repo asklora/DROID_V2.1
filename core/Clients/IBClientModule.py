aa3a67b23cf94fffb9f358560e255d1b8584b2eb
= <=>
2021-08-25 15:42:35 +0700

    update IBClient module

--- core/Clients/IBClientModule.py
+++ core/Clients/IBClientModule.py
@@ -1,11 +1,12 @@
 import requests
 import json
 
-class Client():
-
+class IBClient():
 	def __init__(self):
 		gateway_host = "https://47.242.9.23"
 		gateway_port = f"5000"
+		# gateway_host = "https://2.tcp.ngrok.io"
+		# gateway_port = f"16229"
 
 		self.gateway_url = f"{gateway_host}:{gateway_port}"
 		self.json_headers = {
@@ -41,6 +42,14 @@ class Client():
 				return False, message
 		return False, None
 
+	def check_status(self):
+		url = f"{self.gateway_url}/v1/portal/iserver/auth/status"
+		status, response = self.request_data('get', url)
+		if not status:
+			return "Session did not created"
+		else:
+			return response
+
 	def get_account(self):
 		endpoint = self.gateway_url+"/v1/portal/iserver/accounts"
 		status, message = self.request_data(method='GET', endpoint=endpoint)
@@ -58,6 +67,52 @@ class Client():
 				messages.append(msg['id'])
 		print(messages)
 		return messages
+
+	def account_legder(self,account_id=None):
+		if not account_id:
+			account_id = self.get_account() 
+			if "All" in account_id:
+				account_id.remove('All')
+			if f'95% group' in account_id:
+				account_id.remove(f'95% group')
+		if isinstance(account_id, str):
+			url = f"{self.gateway_url}/v1/api/portfolio/{account_id}/ledger"
+			status, response = self.request_data('get', url)
+		else:
+			response = []
+			status = True
+			for account in account_id:
+				url = f"{self.gateway_url}/v1/api/portfolio/{account}/ledger"
+				status, resp = self.request_data('get', url)
+				response.append(resp)
+				if not status:
+					break
+
+		if status:
+			result = {}
+			if isinstance(response, list):
+				print("it's a list")
+				for resp in response:
+					for key, val in resp.items():
+						if key in result:
+							result[key] += val['cashbalance']
+						else:
+							result[key] = val['cashbalance']
+			else:
+				for key, val in response.items():
+					result[key] = val['cashbalance']
+			print(f"------ result ----- \n {json.dumps(result, indent=4)}")
+			return result
+		else:
+			return "Failed to fetch data"
+
+	def portofolio_account(self):
+		url = f"{self.gateway_url}/v1/api/portfolio/accounts"
+		status, response = self.request_data("get", url)
+		if status:
+			return response
+		else:
+			return "Failed to fetch data"
 
 	"""
 	def find_contract(self, ticker_id):
@@ -90,21 +145,22 @@ class Client():
 	def find_contract(self, ticker_id):
 		url = f"{self.gateway_url}/v1/api/trsrv/stocks?symbols={ticker_id}"
 		status, response = self.request_data('get',url)
-		response = None
+		print(response)
+		resp = None
 		if status:
 			contracts = response[ticker_id]
 			for contract in contracts:
-				for data in contract:
-					if data['assetClass'] == "STK":
-						for con in data['contracts']:
-							if con['isUS']:
-								response = con
-			return response
+				if contract['assetClass'] == "STK":
+					for con in contract['contracts']:
+						if con['isUS']:
+							resp = con
+			# print(f"resp >> {resp}")
+			return resp
 		else:
 			return None
 
 
-	def limit_order(self, account_id, con_order_id, price, qty, conID=None, ticker=None):
+	def limit_order(self, account_id, con_order_id, price, qty, side, conID=None, ticker=None):
 		if account_id not in self.get_account():
 			return "Account ID is not registered"
 		else:
@@ -123,7 +179,7 @@ class Client():
 			"cOID": con_order_id,
 			"orderType": "LMT",
 			"price": price,
-			"side": "BUY",
+			"side": side,
 			"quantity": qty,
 			"tif": "DAY"
 		}
@@ -135,16 +191,21 @@ class Client():
 		else:
 			return message
 
-	def market_order(self, qty, account_id, con_order_id, conID=None, ticker=None):
-		if account_id not in self.get_account():
-			return "Account ID is not registered"
+	def market_order(self, qty, account_id, con_order_id, side, conID=None, ticker=None):
+		list_of_account = self.get_account()
+		if account_id not in list_of_account:
+			msg = "Account ID is not registered"
+			print(msg)
+			return msg
 		else:
 			if not conID:
 				if not ticker:
-					return "please submit your ticker symbol or contract id"
+					msg = "please submit your ticker symbol or contract id"
+					return msg
 				list_of_contract = self.find_contract(ticker)
 				if not list_of_contract:
-					return "Failed to fetch data"
+					msg = "Failed to fetch data"
+					return msg
 				conID = list_of_contract['conid']
 
 				"""==== this is just debug for AAPL stock, comment out this pat and you should choose the conrtact id ===="""
@@ -157,17 +218,18 @@ class Client():
 
 			endpoint = f'{self.gateway_url}/v1/api/iserver/account/{account_id}/order'
 			if isinstance(qty, int):
+				print("----------------------- initiate payload -----------------------")
 				payload = {
 						"conid": conID,
 						"secType": f"{conID}:STK",
 						"cOID": con_order_id,
 						"orderType": "MKT",
-						"side": "BUY",
+						"side": side,
 						"quantity": qty,
 						"tif": "DAY"
 					}
 				payload = json.dumps(payload)
-				status, mesage = self.request_data(method='post', endpoint=endpoint, data=payload)
+				status, message = self.request_data(method='post', endpoint=endpoint, data=payload)
 				if status:
 					print("Success")
 					print(message)
@@ -175,11 +237,12 @@ class Client():
 					print("Failed")
 				return message
 			else:
-				return f"QTY must in integer not in {type(qty)}"
+				msg = f"QTY must in integer not in {type(qty)}"
+				return msg
 
 	def order_status(self, orderId):
 		url = f"{self.gateway_url}/v1/api/iserver/account/order/status/{orderId}"
-		status, response = self.request_data("get")
+		status, response = self.request_data("get", url)
 		if status:
 			return response
 		else:
@@ -193,7 +256,7 @@ class Client():
 		else:
 			return "Failed to fetch data"
 
-	def get_position(self, account_id, conID, orderId):
+	def get_position(self, account_id, conID):
 		url = f"{self.gateway_url}/v1/api/portfolio/{account_id}/positions/{conID}"
 		stat,res =self.request_data('get', url)
 		print(stat,res)
@@ -202,13 +265,6 @@ class Client():
 		else:
 			return "Failed to fetch data"
 
-	def portofolio_account(self):
-		url = f"{self.gateway}/v1/api/portfolio/accounts"
-		status, response = self.request_data("get", url)
-		if status:
-			return response
-		else:
-			return "Failed to fetch data"
 
 
 # if __name__=="__main__":
