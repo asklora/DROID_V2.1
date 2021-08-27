5d6b09f62a4cfe4ef67363d9d4fdf978bd855a46
redloratech <rede.akbar@loratechai.com>
2021-08-27 17:50:06 +0700

    versioning [ci deploy]

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from general.date_process import datetimeNow
 from ingestion.mongo_migration import firebase_user_update
 
 
+
 if __name__ == "__main__":
     user_id = [108, 109, 110]
     # currency_code = ["KRW"]
