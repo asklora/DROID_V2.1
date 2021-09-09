782f7d5ccbdd48f636e7f631f604f344d0c60a50
Agustian <agustian@loratechai.com>
2021-09-09 16:50:26 +0700

    latest price to latest

--- portfolio/daily_hedge_user.py
+++ portfolio/daily_hedge_user.py
@@ -179,7 +179,7 @@ def user_position_check(position_uid, to_date=None, tac=False, hedge=False, late
             if(not status and trading_day <= lastest_price_data.last_date and exp_date >= lastest_price_data.last_date):
                 trading_day = lastest_price_data.last_date
                 print(f"latest price {trading_day} done")
-                status, order_id = create_performance(lastest_price_data, position, latest_price=True)
+                status, order_id = create_performance(lastest_price_data, position, latest=True)
                 if order_id:
                     order = Order.objects.get(order_uid=order_id)
                     log_time = lastest_price_data.intraday_time
