b0b7795c7e455ba19eaa6999e1e12d7908a7e21a
Agustian <agustian@loratechai.com>
2021-09-14 13:03:01 +0700

    Converter

--- core/user/convert.py
+++ core/user/convert.py
@@ -6,34 +6,22 @@ class Convert():
     def convert_money(self):
         pass
 
-class ConvertMoney(Convert):
+class Convert(Convert):
     
+    def __init__(self, from_cur, to_cur):
+        if(type(from_cur) == str):
+            self.from_currency = Currency.objects.get(currency_code=from_cur.upper())
+        elif(type(from_cur) == Currency):
+            self.from_currency = from_cur
+        else:
+            self.from_currency = Currency.objects.get(currency_code="USD")
 
-    def __init__(self,from_cur:str,to_cur:str):
-        """
-        init only define once and only called when the class invoked
-        ex: ConvertMoney()
-
-
-        define from cur and to cur with dependency injection from django models
-
-        self.variable_name -> this can be called to entire class method,so we adjust one at the time in init
-        now we have:
-        - from_cur
-        - to_cur
-        - exchange_rate
-        how it will called :
-            usd_to_eur = ConvertMoney('USD','EUR') 
-            usd_to_hkd = ConvertMoney('USD','HKD')
-            # now we have converter object for use
-
-            # and we can use this repeatedly without query again to the DB, except new converter
-            usd_to_eur.convert(500)
-            usd_to_hkd.convert(100)
-        """
-        # TODO: add exception handler
-        self.from_currency = Currency.objects.get(currency_code=from_cur.upper())
-        self.to_currency = Currency.objects.get(currency_code=to_cur.upper())
+        if(type(to_cur) == str):
+            self.to_currency = Currency.objects.get(currency_code=to_cur.upper())
+        elif(type(to_cur) == Currency):
+            self.to_currency = to_cur
+        else:
+            self.to_currency = Currency.objects.get(currency_code="USD")
         self.exchange_rate = self.from_currency.last_price * self.to_currency.last_price
 
 
