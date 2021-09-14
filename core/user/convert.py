f3b830e2e820a8a14daf6b6b9db4597c6fb2e769
redloratech <rede.akbar@loratechai.com>
2021-09-14 12:46:04 +0700

    converter money

--- core/user/convert.py
+++ core/user/convert.py
@@ -8,46 +8,34 @@ class Convert():
 
 class ConvertMoney(Convert):
     
-    def __init__(self,*args,**kwargs):
-        for key, value in kwargs.items():
-            print(key)
-            print(value)
-            setattr(self, key, value)
 
-    def __init__(self):
-        from_currency = Currency.objects.get(currency_code="USD")
-        to_currency = Currency.objects.get(currency_code="USD")
-        self.from_currency = from_currency
-        self.to_currency = to_currency
-        self.exchange_rate = from_currency.last_price * to_currency.last_price
-
-    def __init__(self, from_currency:str, to_currency:str):
-        from_currency = Currency.objects.get(currency_code=from_currency)
-        to_currency = Currency.objects.get(currency_code=to_currency)
-        self.from_currency = from_currency
-        self.to_currency = to_currency
-        self.exchange_rate = from_currency.last_price * to_currency.last_price
-    
-    def __init__(self, from_currency:Currency, to_currency:Currency):
-        self.from_currency = from_currency
-        self.to_currency = to_currency
-        self.exchange_rate = from_currency.last_price * to_currency.last_price
-
-    def run(self):
+    def __init__(self,from_cur:str,to_cur:str):
         """
-        in here we delete the long if else statement
-        will trigger the function inside this class with prefix name and invokes
+        init only define once and only called when the class invoked
+        ex: ConvertMoney()
+
+
+        define from cur and to cur with dependency injection from django models
+
+        self.variable_name -> this can be called to entire class method,so we adjust one at the time in init
+        now we have:
+        - from_cur
+        - to_cur
+        - exchange_rate
+        how it will called :
+            usd_to_eur = ConvertMoney('USD','EUR') 
+            usd_to_hkd = ConvertMoney('USD','HKD')
+            # now we have converter object for use
+
+            # and we can use this repeatedly without query again to the DB, except new converter
+            usd_to_eur.convert(500)
+            usd_to_hkd.convert(100)
         """
-        func_name =f'on_{self.instance.side}_{self.instance.status}'
-        
-        if hasattr(self,func_name):
-            """
-            only invoke this function.
-            hasattr will check the function name is exist or not 
-           - on_buy_placed
-            """
-            function = getattr(self, func_name)
-            function()
+        # TODO: add exception handler
+        self.from_currency = Currency.objects.get(currency_code=from_cur.upper())
+        self.to_currency = Currency.objects.get(currency_code=to_cur.upper())
+        self.exchange_rate = self.from_currency.last_price * self.to_currency.last_price
+
 
     def convert_money(self, amount):
         rounded = 0
@@ -59,26 +47,26 @@ class ConvertMoney(Convert):
             result = amount / self.exchange_rate
         return round(result, rounded)
     
-    def convert_money(self, amount, from_currency:str, to_currency:str):
-        from_currency = Currency.objects.get(currency_code=from_currency)
-        to_currency = Currency.objects.get(currency_code=to_currency)
-        exchange_rate = from_currency.last_price * to_currency.last_price
-        rounded = 0
-        if(to_currency.is_decimal):
-            rounded = 2
-        if(from_currency.last_price <= to_currency.last_price):
-            result = amount * exchange_rate
-        else:
-            result = amount / exchange_rate
-        return round(result, rounded)
+    # def convert_money(self, amount, from_currency:str, to_currency:str):
+    #     from_currency = Currency.objects.get(currency_code=from_currency)
+    #     to_currency = Currency.objects.get(currency_code=to_currency)
+    #     exchange_rate = from_currency.last_price * to_currency.last_price
+    #     rounded = 0
+    #     if(to_currency.is_decimal):
+    #         rounded = 2
+    #     if(from_currency.last_price <= to_currency.last_price):
+    #         result = amount * exchange_rate
+    #     else:
+    #         result = amount / exchange_rate
+    #     return round(result, rounded)
     
-    def convert_money(self, amount, from_currency:Currency, to_currency:Currency):
-        exchange_rate = from_currency.last_price * to_currency.last_price
-        rounded = 0
-        if(to_currency.is_decimal):
-            rounded = 2
-        if(from_currency.last_price <= to_currency.last_price):
-            result = amount * exchange_rate
-        else:
-            result = amount / exchange_rate
-        return round(result, rounded)
+    # def convert_money(self, amount, from_currency:Currency, to_currency:Currency):
+    #     exchange_rate = from_currency.last_price * to_currency.last_price
+    #     rounded = 0
+    #     if(to_currency.is_decimal):
+    #         rounded = 2
+    #     if(from_currency.last_price <= to_currency.last_price):
+    #         result = amount * exchange_rate
+    #     else:
+    #         result = amount / exchange_rate
+    #     return round(result, rounded)
