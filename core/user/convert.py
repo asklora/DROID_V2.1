e891ee052c8b7f4760a682486e1e3bfecb2e794c
Agustian <agustian@loratechai.com>
2021-09-14 11:38:15 +0700

    Convert Money

--- core/user/convert.py
+++ core/user/convert.py
@@ -10,8 +10,29 @@ class ConvertMoney(Convert):
     
     def __init__(self,*args,**kwargs):
         for key, value in kwargs.items():
+            print(key)
+            print(value)
             setattr(self, key, value)
+
+    def __init__(self):
+        from_currency = Currency.objects.get(currency_code="USD")
+        to_currency = Currency.objects.get(currency_code="USD")
+        self.from_currency = from_currency
+        self.to_currency = to_currency
+        self.exchange_rate = from_currency.last_price * to_currency.last_price
+
+    def __init__(self, from_currency:str, to_currency:str):
+        from_currency = Currency.objects.get(currency_code=from_currency)
+        to_currency = Currency.objects.get(currency_code=to_currency)
+        self.from_currency = from_currency
+        self.to_currency = to_currency
+        self.exchange_rate = from_currency.last_price * to_currency.last_price
     
+    def __init__(self, from_currency:Currency, to_currency:Currency):
+        self.from_currency = from_currency
+        self.to_currency = to_currency
+        self.exchange_rate = from_currency.last_price * to_currency.last_price
+
     def run(self):
         """
         in here we delete the long if else statement
@@ -28,6 +49,15 @@ class ConvertMoney(Convert):
             function = getattr(self, func_name)
             function()
 
+    def convert_money(self, amount):
+        rounded = 0
+        if(self.to_currency.is_decimal):
+            rounded = 2
+        if(self.from_currency.last_price <= self.to_currency.last_price):
+            result = amount * self.exchange_rate
+        else:
+            result = amount / self.exchange_rate
+        return round(result, rounded)
     
     def convert_money(self, amount, from_currency:str, to_currency:str):
         from_currency = Currency.objects.get(currency_code=from_currency)
