7d2aab1c0cfef996702a0d537693e8b2198be393
redloratech <rede.akbar@loratechai.com>
2021-09-15 14:46:49 +0700

    [ci deploy]

--- ingestion/mongo_migration.py
+++ ingestion/mongo_migration.py
@@ -405,7 +405,7 @@ async def do_task(position_data:pd.DataFrame, bot_option_type:pd.DataFrame, user
 def firebase_user_update(user_id=None, currency_code=None):
     if not user_id and not currency_code:
         return
-    print("Start User Populate")
+    print("Start Populate portfolio")
     bot_type = get_bot_type()
     bot_option_type = get_bot_option_type()
     bot_option_type = bot_option_type.merge(bot_type, how="left", on=["bot_type"])
