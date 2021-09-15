50c0d7417edbf5a51418d850df1fe8adb8831550
redloratech <rede.akbar@loratechai.com>
2021-09-15 16:11:54 +0700

    [ci deploy]

--- core/djangomodule/management/commands/celery_publish.py
+++ core/djangomodule/management/commands/celery_publish.py
@@ -1,9 +1,19 @@
 from django.core.management.base import BaseCommand
 from config.celery import debug_task
-
+from datasource.rkd import RkdStream
+import time
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        debug_task.apply_async(queue='broadcaster')
+        # debug_task.apply_async(queue='broadcaster')
+        rkd = RkdStream()
+        rkd.chanels = 'market'
+        proc = rkd.thread_stream_quote()
+        proc.daemon = True
+        proc.start()
+        
+        while True:
+            print('ok')
+            time.sleep(5)
         # app.send_task("djangomodule.aoa.apa", kwargs={
         #               "payload": "222"}, queue='javascript')
