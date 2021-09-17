784a3f4a67efa86b3dd5a980673fc1c72984ef3a
redloratech <rede.akbar@loratechai.com>
2021-09-17 23:22:52 +0700

    test

--- deployment/script/hook.sh
+++ deployment/script/hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -H "Authorization: token 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" \
-    --request POST \
-    --data '{"event_type": "build_image"}' \
-    https://api.github.com/repos/asklora/DROID_V2.1/dispatches
+curl -S -o POST -H "Authorization: token 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" \
+-H "Accept: application/vnd.github.v3+json"\
+    https://api.github.com/repos/asklora/DROID_V2.1/dispatches \
+    --data '{"event_type": "build_image"}'
