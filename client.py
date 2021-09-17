31540987fa6fae5cb89ee92601c7ca89e3b73e15
redloratech <rede.akbar@loratechai.com>
2021-09-17 23:54:18 +0700

    test

--- client.py
+++ client.py
@@ -1,15 +1,23 @@
-from websocket import create_connection
+# from websocket import create_connection
+# import json
+# ws = create_connection('wss://services.asklora.ai/ws/market/')
+# ws.send(json.dumps({'type':'streaming','message':'i need to stream'}))
+# while True:
+#     payload = json.loads(ws.recv())
+#     print(payload)
+#     if payload['message'] == 'PING':
+#         print('reply')
+#         ws.send(json.dumps({
+#             'type':'ping',
+#             'message':'PONG',
+#             'user':'python-client'
+#         }))
+import requests
 import json
-ws = create_connection('wss://services.asklora.ai/ws/market/')
-ws.send(json.dumps({'type':'streaming','message':'i need to stream'}))
-while True:
-    payload = json.loads(ws.recv())
-    print(payload)
-    if payload['message'] == 'PING':
-        print('reply')
-        ws.send(json.dumps({
-            'type':'ping',
-            'message':'PONG',
-            'user':'python-client'
-        }))
-
+data = json.dumps({"event_type": "build_image"})
+response =requests.post('https://api.github.com/repos/asklora/DROID_V2.1',data=data,headers={
+    "Authorization": "token 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",
+    "Accept": "application/vnd.github.v3+json"
+})
+print(response.json())
+print(response.status_code)
