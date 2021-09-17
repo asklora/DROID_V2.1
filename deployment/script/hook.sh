e6614e411641348afca272f5c43023ce19bafa3b
redloratech <rede.akbar@loratechai.com>
2021-09-17 23:23:55 +0700

    test

--- deployment/script/hook.sh
+++ deployment/script/hook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -S -o POST -H "Authorization: token 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" \
+curl -o POST -H "Authorization: token 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" \
 -H "Accept: application/vnd.github.v3+json"\
     https://api.github.com/repos/asklora/DROID_V2.1/dispatches \
     --data '{"event_type": "build_image"}'
