e36b9af72232d0e8382777bd020b29a76742a792
redloratech <rede.akbar@loratechai.com>
2021-09-18 16:58:29 +0700

    midleware check

--- config/settings.py
+++ config/settings.py
@@ -96,6 +96,7 @@ INSTALLED_APPS = DJANGO_DEFAULT_APPS + ADDITIONAL_APPS + CORE_APPS
 Django middleware settings
 """
 MIDDLEWARE = [
+    "config.middleware.HealthCheck.HealthCheckMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
