c4d7cae557268b0d95882841a5e3834797998b26
redloratech <rede.akbar@loratechai.com>
2021-09-18 22:26:12 +0700

    test-noimage

--- deployment/script/codedeploy/stop_server.sh
+++ deployment/script/codedeploy/stop_server.sh
@@ -2,4 +2,5 @@
 docker-compose -p droid -f deployment/compose/droid.yml down
 docker system prune -a -f && docker image prune -a -f 
 sudo rm -rf /home/ubuntu/DROID_V2.1
+mkdir DROID_V2.1
 # cd /home/ubuntu/ && git clone git@github.com:asklora/DROID_V2.1.git
