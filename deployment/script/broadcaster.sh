6088f9f159cb42e9f8ff4e32462734101ee03b7e
redloratech <rede.akbar@loratechai.com>
2021-09-29 17:13:33 +0700

    build-base-image

--- deployment/script/broadcaster.sh
+++ deployment/script/broadcaster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=config.settings.production
-celery -A core.services worker -l INFO  --concurrency=20 --hostname=broadcaster@%h -Q broadcaster
+celery -A core.services worker -l INFO  --concurrency=5 --hostname=broadcaster@%h -Q broadcaster
