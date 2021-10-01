80bbfd499230b8d524ed8c0b05b990137c0cd3ff
redloratech <rede.akbar@loratechai.com>
2021-10-01 11:19:17 +0700

    test

--- core/djangomodule/management/commands/cancel_pending_order.py
+++ core/djangomodule/management/commands/cancel_pending_order.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
-from datetime import relativedelta,datetime
+from datetime import timedelta,datetime
 from core.services.order_services import cancel_pending_order
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        date = datetime.now() - relativedelta(days=1)
+        date = datetime.now() - timedelta(days=1)
         cancel_pending_order(from_date=date)
 
 
