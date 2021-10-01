5d140ce35fe3d93f8319f6497871baf0f33dfdb0
redloratech <rede.akbar@loratechai.com>
2021-10-01 13:08:13 +0700

    test-noimage

--- deployment/script/codebuild/after_test.sh
+++ deployment/script/codebuild/after_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [[ '$CODEBUILD_BUILD_SUCCEEDING'=='1' ]] then 
+if [[ '$CODEBUILD_BUILD_SUCCEEDING'=='1' ]]
+then 
 python manage.py send_slack_message -m 'Test Success :white_check_mark:' --settings=config.settings.production 
 else
 python manage.py send_slack_message -m "Test Fail :x: rollback update" --settings=config.settings.production
