29a3a17a94f9ec7dd0ecba1d33b35032ab447b33
Agustian <agustian@loratechai.com>
2021-10-01 15:34:36 +0700

    int to float

--- ingestion/firestore_migration.py
+++ ingestion/firestore_migration.py
@@ -457,6 +457,10 @@ def firebase_user_update(user_id=None, currency_code=None):
         user_core = user_core.merge(stock_order_pending, how="left", on=["user_id"])
         user_core["stock_pending_amount"] = np.where(user_core["stock_pending_amount"].isnull(), 0.0, user_core["stock_pending_amount"])
         user_core["pending_amount"] = user_core["stock_pending_amount"] + user_core["bot_pending_amount"]
+        
+        user_core["bot_pending_amount"]  = user_core["bot_pending_amount"].astype(float)
+        user_core["stock_pending_amount"]  = user_core["stock_pending_amount"].astype(float)
+        user_core["pending_amount"]  = user_core["pending_amount"].astype(float)
         # print(user_core)
         # import sys
         # sys.exit(1)
