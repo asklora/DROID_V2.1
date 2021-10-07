b9218de576ce4570c19615581893d28352ea8f63
Hendika Nugroho <hendika.nugroho@loratechai.com>
2021-10-07 12:04:36 +0700

    testbuild

--- tests/integration/sell_order/test_sell_convert.py
+++ tests/integration/sell_order/test_sell_convert.py
@@ -21,13 +21,13 @@ def test_sell_order_with_conversion(user):
     amount: float = 10000
 
     # converter to usd
-    usd_conversion = ConvertMoney(user.currency, "USD")
-    usd_conversion_result = usd_conversion.convert(amount)
+    conversion = ConvertMoney(user.currency, "USD")
+    conversion_result = conversion.convert(amount)
 
     order: Order = create_buy_order(
         price=price,
         ticker=ticker,
-        amount=usd_conversion_result,
+        amount=conversion_result,
         margin=2,
         user_id=user.id,
         bot_id="UNO_OTM_007692",
@@ -60,10 +60,6 @@ def test_sell_order_with_conversion(user):
     confirmed_sell_order = Order.objects.get(pk=sell_order.pk)
     assert confirmed_sell_order
 
-    # the one we are testing
-    hkd_conversion = ConvertMoney(confirmed_sell_order.amount, "HKD")
-    hkd_conversion_result = hkd_conversion.convert(amount)
-
     confirmed_sell_order.status = "placed"
     confirmed_sell_order.placed = True
     confirmed_sell_order.placed_at = datetime.now()
@@ -73,5 +69,5 @@ def test_sell_order_with_conversion(user):
 
     assert (
         confirmed_sell_order.setup["position"]["bot_cash_balance"]
-        == hkd_conversion_result
+        == conversion_result
     )
