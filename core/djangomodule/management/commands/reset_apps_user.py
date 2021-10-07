607a37bbc566161f6bd5b4289f2728fd77fec8de
redloratech <rede.akbar@loratechai.com>
2021-10-07 11:00:37 +0700

    command update

--- core/djangomodule/management/commands/reset_apps_user.py
+++ core/djangomodule/management/commands/reset_apps_user.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand
 from core.user.models import User
+from core.djangomodule.crudlib.user import sync_delete_user
 from general.firestore_query import delete_firestore_user
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
         users  = User.objects.filter(current_status='verified')
         for user in users:
-            user_id=user.id
-            user.delete()
-            delete_firestore_user(user_id)
+            payload = {'username':user.username}
+            sync_delete_user(payload)
