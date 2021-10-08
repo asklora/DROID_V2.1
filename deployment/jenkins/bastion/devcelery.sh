e0602542599dbba86d2669b2eb7981ea1901d341
redloratech <rede.akbar@loratechai.com>
2021-10-08 09:48:55 +0700

    dev-run

--- deployment/jenkins/bastion/devcelery.sh
+++ deployment/jenkins/bastion/devcelery.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=config.settings.development
+export CELERY_ROLE=master
 celery -A core.services worker --beat -l  INFO --hostname=dev-master@%h --concurrency=12 --scheduler django_celery_beat.schedulers:DatabaseScheduler
