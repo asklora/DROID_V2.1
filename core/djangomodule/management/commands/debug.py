eac2748f8c185a1abc9e8b1d3bee96aa26b0bde7
Agustian <agustian@loratechai.com>
2021-10-14 13:13:18 +0700

    Debug

--- core/djangomodule/management/commands/debug.py
+++ core/djangomodule/management/commands/debug.py
@@ -1,6 +1,8 @@
+from ingestion.firestore_migration import firebase_user_update
+from bot.calculate_bot import populate_daily_profit
 from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Process")
-        # populate_daily_profit()
-        # firebase_user_update()
+        populate_daily_profit()
+        firebase_user_update()
