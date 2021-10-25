ffc8cc7672b082c14a9bf11cb64f5e4c6d6e650d
redloratech <rede.akbar@loratechai.com>
2021-10-25 13:24:33 +0700

    add cli installer

--- deployment/script/localcelery.sh
+++ deployment/script/localcelery.sh
@@ -2,4 +2,4 @@
 export DJANGO_SETTINGS_MODULE=config.settings.sandbox
 export CELERY_ROLE=master
 python manage.py migrate --settings=config.settings.sandbox
-celery -A core.services worker -l  INFO --hostname=master@%h --concurrency=5
+celery -A core.services worker --beat -l  INFO --hostname=master@%h --concurrency=5 --scheduler django_celery_beat.schedulers:DatabaseScheduler
