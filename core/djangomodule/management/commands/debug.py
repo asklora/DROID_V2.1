7cbb934c0d0574da296890826a5a87e0965ecc12
redloratech <rede.akbar@loratechai.com>
2021-10-27 12:22:19 +0700

    fix dev-ru

--- core/djangomodule/management/commands/debug.py
+++ core/djangomodule/management/commands/debug.py
@@ -3,11 +3,14 @@ from bot.calculate_bot import populate_daily_profit
 from django.core.management.base import BaseCommand
 from core.services.tasks import daily_hedge_user
 from datasource.rkd import RkdData
+from core.universe.models import Universe
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Process")
+        ticker = list(Universe.objects.filter(currency_code__in=["HKD","USD"], 
+                    is_active=True).exclude(Error__contains='{').values_list('ticker',flat=True))
         rkd = RkdData()
-        print(rkd.get_quote(['MSFT.O','0586.HK'], df=True,save=True))
+        print(rkd.get_snapshot(ticker,save=True))
         # populate_daily_profit()
         # firebase_user_update()
         # firebase_universe_update(currency_code=["HKD"])
