5dafd3acff38f2ce5d917d96d10665124074edfc
redloratech <rede.akbar@loratechai.com>
2021-11-02 14:42:31 +0700

    AL1-11:market test check

--- core/services/exchange_services.py
+++ core/services/exchange_services.py
@@ -4,15 +4,30 @@ from core.universe.models import ExchangeMarket
 from datetime import datetime
 import subprocess
 import os
-
+from django.conf import settings
 def restart_worker():
     envrion = os.environ.get('DJANGO_SETTINGS_MODULE', False)
     if envrion in ['config.settings.production','config.settings.prodtest']:
-        subprocess.Popen(['docker', 'restart', 'Celery','CeleryBroadcaster'])
-        return {'response':'restart celery prod'}
-    else:
-        subprocess.Popen(['docker', 'restart', 'Celery'])
-        return {'response':'restart celery staging'}
+        if settings.TESTDEBUG:
+            exchanges = ExchangeMarket.objects.filter(currency_code__in=["HKD","USD"])
+            exchanges = exchanges.filter(group='Core')
+            for exchange in exchanges:
+                market = TradingHours(mic=exchange.mic)
+                market.run_market_check()
+        else:
+            subprocess.Popen(['docker', 'restart', 'Celery','CeleryBroadcaster'])
+        return {'response':'restart celery prod','code':200}
+    elif envrion in ['config.settings.local','config.settings.test','config.settings.development'] :
+        if settings.TESTDEBUG:
+            exchanges = ExchangeMarket.objects.filter(currency_code__in=["HKD","USD"])
+            exchanges = exchanges.filter(group='Core')
+            for exchange in exchanges:
+                market = TradingHours(mic=exchange.mic)
+                market.run_market_check()
+        else:
+            subprocess.Popen(['docker', 'restart', 'Celery'])
+        return {'response':'restart celery staging','code':200}
+    return {'response':'both function not executed','code':400}
 
 
 
@@ -24,7 +39,7 @@ def market_task_checker():
     exchanges = exchanges.filter(group='Core')
     fail = []
     for exchange in exchanges:
-        if exchange.until < datetime.now():
+        if exchange.until_time < datetime.now():
             fail.append(exchange.mic)
     if fail:
         restart_worker()
