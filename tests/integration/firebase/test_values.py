a8cb5461342d1091fb5d8ff3e8ddd30f4c03ac90
Hendika Nugroho <hendika.nugroho@loratechai.com>
2021-11-02 14:06:24 +0700

    Fix firebase values

--- tests/integration/firebase/test_values.py
+++ tests/integration/firebase/test_values.py
@@ -1,77 +1,65 @@
-import time
+from random import choice
+from typing import List
 
-from bot.calculate_bot import populate_daily_profit
+import pytest
+from core.user.models import User
 from django.conf import settings
-from ingestion.firestore_migration import firebase_user_update
-from tests.utils.firebase_schema import FIREBASE_SCHEMA
-from tests.utils.order import (
-    confirm_order,
-    create_buy_order,
-    create_sell_order,
+from django.utils import timezone
+
+pytestmark = pytest.mark.django_db(
+    databases=[
+        "default",
+        "aurora_read",
+        "aurora_write",
+    ]
 )
-from tests.utils.user import set_user_joined
 
 
-def test_user_values(mocker, firestore_client, user, tickers):
+def test_user_values(firestore_client):
+    def get_firestore_doc(id):
+        return firestore_client.collection(
+            settings.FIREBASE_COLLECTION["portfolio"],
+        ).document(
+            str(id),
+        )
+
     def calculate_pct(current_assets: float):
-        # The asset from previous month is 200000 because
-        # it's the first month for the test user
-        # for real accounts, this will be 100000 HKD
-        previous_assets: float = 200000.0
+        previous_assets: float = 100000.0  # for new users, the initial deposit
 
-        print(f"current assets: {current_assets}\nprevious assets: {previous_assets}")
+        percentage: float = (
+            (current_assets - previous_assets) / previous_assets * 100
+        )
 
-        percentage: float = (current_assets - previous_assets) / previous_assets * 100
         return percentage
 
-    # just in case if the user is still not joined the competition
-    if not user.is_joined:
-        set_user_joined(mocker, user)
+    # we filter users that just joined
+    last_month: int = timezone.now().month - 1
+    new_users: List[User] = User.objects.filter(
+        date_joined__month=last_month,
+        is_joined=True,
+    ).exclude(user_balance__amount=100000)
+    assert new_users
 
-    # get random ticker
-    ticker, price = tickers[4].values()
+    # we pick random user
+    user = choice(new_users)
+    print(user.user_balance.__dict__)
 
-    # we create a new order here
-    order = create_buy_order(
-        price=price,
-        ticker=ticker,
-        margin=2,
-        user_id=user.id,
-        bot_id="UNO_OTM_007692",
-    )
-
-    # we confirm the order
-    confirm_order(order)
-
-    # then we sell it, after waiting fo a while
-    sell_position, sell_order = create_sell_order(order)
-    confirm_order(sell_order)
-
-    # update firebases
-    populate_daily_profit()
-    firebase_user_update([user.id])
-
-    # it takes a while to propagate to firebase so give it a second
-    time.sleep(90)
-
-    doc_ref = firestore_client.collection(
-        settings.FIREBASE_COLLECTION["portfolio"],
-    ).document(
-        str(user.id),
-    )
-
+    # we check if the user data has been added to firebase
+    doc_ref = get_firestore_doc(user.id)
     doc = doc_ref.get()
-
     assert doc.exists
 
+    # we then get the data
     doc_dict = doc.to_dict()
     print(doc_dict)
 
-    # whether the data in the firebase is structured correctly
-    assert FIREBASE_SCHEMA.validate(doc_dict)
-
     # assert the daily profit percentage
-    assert doc_dict["daily_profit_pct"] == round(calculate_pct(doc_dict["current_asset"]), 2)
+    firebase_percentage: float = round(doc_dict["daily_profit_pct"], 2)
+    calculated_percentage: float = round(
+        calculate_pct(doc_dict["current_asset"]),
+        2,
+    )
+    assert firebase_percentage == calculated_percentage
 
     # TODO: assert mtd
 
