10782f1bee292311986664811e029c86530c130b
redloratech <rede.akbar@loratechai.com>
2021-12-09 10:42:07 +0700

    class

--- core/services/order_services.py
+++ core/services/order_services.py
@@ -48,7 +48,7 @@ def pending_order_checker(self,currency=None):
                 fb_token=None
                 if 'firebase_token' in order.order_summary:
                     fb_token = order.order_summary['firebase_token']
-                payload = {'order_uid': str(order.order_uid),'status':'placed'}
+                payload = {'order_uid': str(order.order_uid),'status':'placed','side':order.side}
                 if fb_token:
                     payload['firebase_token'] = fb_token
                 
