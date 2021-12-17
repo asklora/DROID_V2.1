e360958f3b2d22de726ed6f061e6b88d25eb12cb
Hendika Nugroho <hendika.nugroho@loratechai.com>
2021-12-17 17:14:23 +0700

    fix test

--- tests/unit/test_translation.py
+++ tests/unit/test_translation.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext
 def test_translation(use_chinese) -> None:
     assert gettext("order not found") != "order not found"
     assert (
-        gettext("order not found") == "Order cannot be found, but in Chinese"
+        gettext("order not found") == "找不到此交易"
     )
