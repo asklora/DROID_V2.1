63ccd5c5e586762d8fe04e2575270ea8ad6d6040
redloratech <rede.akbar@loratechai.com>
2021-12-23 16:56:09 +0700

    test-noimage

--- core/orders/factory/order_protocol.py
+++ core/orders/factory/order_protocol.py
@@ -3,15 +3,6 @@ from typing_extensions import Protocol
 from core.orders.models import Order
 from typing import Union
 
-import RPC
-
-rpc = RPC('http:droid.com')
-
-
-bot_adjuster = rpc.uidgenrator(11,11)
-
-
-
 class ValidatorProtocol(Protocol):
     
     def validate(self):
