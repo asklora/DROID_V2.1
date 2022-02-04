9bad62f3e4a00a9d03e20b6a76a2d35f891ca880
Agustian <agustian@loratechai.com>
2022-02-04 11:45:29 +0700

    Hedging Price

---
+++ core/djangomodule/management/commands/backtest_hedging.py
@@ -0,0 +1,274 @@
+from django.core.management.base import BaseCommand
+import gc
+import logging
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+import pandas as pd
+pd.options.mode.chained_assignment = None
+from tqdm import tqdm
+from general.sql_output import upsert_data_to_database
+from general.date_process import dateNow, droid_start_date
+from general.table_name import get_bot_uno_backtest_table_name
+from bot.data_process import check_start_end_date, check_time_to_exp
+from bot.preprocess import cal_interest_rate, cal_q
+from bot.black_scholes import (find_vol, deltaUnOC)
+from bot.data_download import (
+    get_bot_backtest_data, 
+    get_bot_backtest_data_date_list, 
+    get_master_tac_price, 
+    get_interest_rate_data,
+    get_dividends_data)
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("-run_number", "--run_number", help="run_number", type=int, default=0)
+
+    def handle(self, *args, **options):
+        pass
+
+# *********************** Filling up the Null values **************************
+def shift5_numba(arr, num, fill_value=np.nan):
+    # This function is used for shifting the numpy array
+    result = np.empty_like(arr)
+    if num > 0:
+        result[:num] = fill_value
+        result[num:] = arr[:-num]
+    elif num < 0:
+        result[num:] = fill_value
+        result[:num] = arr[-num:]
+    else:
+        result[:] = arr
+    return result
+
+def fill_bot_backtest_uno(start_date=None, end_date=None, time_to_exp=None, ticker=None, currency_code=None, mod=False, history=False, total_no_of_runs=1, run_number=0):
+    time_to_exp = check_time_to_exp(time_to_exp)
+    start_date, end_date = check_start_end_date(start_date, end_date)
+    # This function is used for filling the nulls in executive options database. Checks whether the options are expired
+    # or knocked out or not triggered at all.
+    tqdm.pandas()
+    dates_df_unique = get_bot_backtest_data_date_list(start_date=start_date, end_date=end_date, time_to_exp=time_to_exp, ticker=ticker, currency_code=currency_code, uno=True, mod=mod, null_filler=True)
+    # Dividing the dates to sections for running manually
+    dates_df_unique = np.sort(dates_df_unique)
+    divison_length = round(len(dates_df_unique) / total_no_of_runs)
+    dates_per_run = [dates_df_unique[x:x + divison_length] for x in range(0, len(dates_df_unique), divison_length)]
+    # **************************************************
+    date_min = min(dates_per_run[run_number])
+    date_max = max(dates_per_run[run_number])
+    # Downloading the options that are not triggered from the executive database.
+    null_df = get_bot_backtest_data(start_date=date_min, end_date=date_max, time_to_exp=time_to_exp, ticker=ticker, currency_code=currency_code, uno=True, mod=mod, null_filler=True)
+    start_date = null_df.spot_date.min()
+    #tac_data = tac_data_download_null_filler(start_date, args)
+    tac_data = get_master_tac_price(start_date=date_min, end_date=date_max, ticker=ticker, currency_code=currency_code)
+    tac_data = tac_data.sort_values(by=["currency_code", "ticker", "trading_day"], ascending=True)
+    interest_rate_data = get_interest_rate_data()
+    dividends_data = get_dividends_data()
+
+    # ********************************************************************************************
+    # ********************************************************************************************
+    prices_df = tac_data.pivot_table(index=tac_data.trading_day, columns="ticker", values="tri_adj_close",aggfunc="first", dropna=False)
+    prices_df = prices_df.ffill()
+    prices_df = prices_df.bfill()
+
+    dates_df = prices_df.index.to_series()
+    dates_df = dates_df.reset_index(drop=True)
+    dates_df = pd.DataFrame(dates_df)
+    dates_df["spot_date_index"] = dates_df.index
+    dates_df["expiry_date_index"] = dates_df.index
+    dates_df = dates_df.rename(columns={"trading_day": "spot_date"})
+    null_df = null_df.merge(dates_df[["spot_date", "spot_date_index"]], on=["spot_date"], how="left")
+    dates_df = dates_df.rename(columns={"spot_date": "expiry_date"})
+    null_df = null_df.merge(dates_df[["expiry_date", "expiry_date_index"]], on=["expiry_date"], how="left")
+    null_df["expiry_date_index"].fillna(-2, inplace=True)
+
+    prices_ind_df = pd.DataFrame(prices_df.columns)
+    prices_ind_df["ticker_index"] = prices_ind_df.index
+    null_df = null_df.merge(prices_ind_df[["ticker", "ticker_index"]], on=["ticker"])
+    null_df = null_df.sort_values(by="expiry_date", ascending=False)
+
+    prices_np = prices_df.values
+    dates_np = dates_df.values
+    del prices_df, tac_data
+    gc.collect()
+    table_name = get_bot_uno_backtest_table_name()
+    if mod:
+        table_name += "_mod"
+    def exec_fill_fun(row, prices_np, dates_np, null_df):
+        # Calculate the desired quantities row by row.
+        # Everything is converted to numpy array for faster runtime.
+        if row.expiry_date_index == -2:
+            row.expiry_date_index = prices_np.shape[0] - 1
+        prices_temp = prices_np[int(row.spot_date_index):int(row.expiry_date_index+1), int(row.ticker_index)]
+        if len(prices_temp) == 0:
+            return row
+        dates_temp = dates_np[int(row.spot_date_index):int(row.expiry_date_index+1), 0]
+        t = np.full((len(prices_temp)), ((row["expiry_date"] - dates_temp).astype("timedelta64[D]")) / np.timedelta64(1, "D"))
+        strike = np.full((len(prices_temp)), row["strike"])
+        barrier = np.full((len(prices_temp)), row["barrier"])
+
+        cond = (null_df.ticker == row.ticker) & (null_df.spot_date >= row.spot_date) &\
+               (null_df.spot_date <= row.expiry_date) & (null_df.time_to_exp == row.time_to_exp) &\
+               (null_df.option_type == row.option_type)
+
+        dates_df2 = pd.DataFrame(dates_temp, columns=["spot_date"])
+        null_df_temp = null_df[cond]
+        null_df_temp = null_df_temp.drop_duplicates(subset="spot_date", keep="last")
+        null_df_temp = dates_df2.merge(null_df_temp, on="spot_date", how="left")
+        null_df_temp = null_df_temp.bfill().ffill()
+
+        atmv_0 = null_df_temp["atm_volatility_spot"].values
+        atmv_1Y = null_df_temp["atm_volatility_one_year"].values
+        atmv_inf = null_df_temp["atm_volatility_infinity"].values
+        skew_1m = null_df_temp["slope"].values
+        skew_inf = null_df_temp["slope_inf"].values
+        curv_1m = null_df_temp["deriv"].values
+        curv_inf = null_df_temp["deriv_inf"].values
+
+        days_to_expiry = np.full((len(prices_temp)),((row["expiry_date"] - dates_temp).astype("timedelta64[D]")) / np.timedelta64(1, "D"))
+
+        r = cal_interest_rate(interest_rate_data[interest_rate_data.currency_code == row.currency_code], days_to_expiry)
+        q = cal_q(row, dividends_data[dividends_data.ticker == row.ticker], dates_temp, prices_temp)
+        v1 = find_vol(np.divide(strike, prices_temp), t, atmv_0, atmv_1Y, atmv_inf,12, skew_1m, skew_inf, curv_1m, curv_inf, r, q)
+        v2 = find_vol(np.divide(barrier, prices_temp), t, atmv_0, atmv_1Y, atmv_inf,12, skew_1m, skew_inf, curv_1m, curv_inf, r, q)
+        stock_balance = deltaUnOC(prices_temp, strike, barrier, (barrier - strike), t, r, q, v1, v2)
+        stock_balance = np.nan_to_num(stock_balance)
+        last_hedge = np.copy(stock_balance)
+        last_hedge = shift5_numba(last_hedge, 1)
+        last_hedge = np.nan_to_num(last_hedge)
+        hedge = 0.05
+
+        # Condition
+        condition1A = prices_temp > strike
+        condition2A = np.abs(last_hedge - stock_balance) <= 0.05
+        conditionA = condition1A & condition2A
+        condition1B = prices_temp <= strike
+        condition2B = np.abs(last_hedge - stock_balance) <= 0.01
+        conditionB = condition1B & condition2B
+        condition = conditionA | conditionB
+        stock_balance[condition] = 2
+        stock_balance = fill_zeros_with_last(stock_balance)
+        barrier_indices = np.argmax((prices_temp >= row.barrier))
+        stock_balance2 = np.copy(stock_balance)
+        stock_balance2 = shift5_numba(stock_balance2, 1)
+        stock_balance2 = np.nan_to_num(stock_balance2)
+
+        if barrier_indices != 0:
+            # barrier(knockout) is triggered.
+            row.event = "KO"
+            row["stock_balance"] = stock_balance[barrier_indices]
+            row["stock_price"] = prices_temp[barrier_indices]
+            row["now_price"] = prices_temp[barrier_indices]
+            row["event_date"] = dates_temp[barrier_indices]
+            row["event_price"] = prices_temp[barrier_indices]
+            row["expiry_price"] = prices_temp[-1]
+            row["now_date"] = dates_temp[barrier_indices]
+            row["expiry_payoff"] = row["barrier"] - row["strike"]
+            row["expiry_return"] = (prices_temp[-1] / prices_temp[0]) - 1
+            row["drawdown_return"] = np.amin(prices_temp) / prices_temp[0] - 1
+            row["duration"] = (row["event_date"] - row["spot_date"]).days / 365
+            row["r"] = r[barrier_indices]
+            row["q"] = q[barrier_indices]
+            row["v1"] = v1[barrier_indices]
+            row["v2"] = v2[barrier_indices]
+            row["t"] = t[barrier_indices]
+            stock_balance[barrier_indices] = 0
+            pnl = (stock_balance2 - stock_balance) * prices_temp
+            churn = (stock_balance2 - stock_balance)
+            pnl = pnl[:barrier_indices+1]
+            churn = churn[:barrier_indices+1]
+            row["pnl"] = np.nansum(pnl)
+            delta_churn = np.abs(churn)
+            row["delta_churn"] = np.nansum(delta_churn)
+            row["bot_return"] = row["pnl"] / prices_temp[0]
+            row["num_hedges"] = np.sum(stock_balance2[:barrier_indices+1] != stock_balance[:barrier_indices+1])
+
+        elif dates_temp[-1] == row["expiry_date"]:
+            # Expiry is triggered.
+            row.event = "expire"
+            row["stock_balance"] = stock_balance[-1]
+            row["stock_price"] = prices_temp[-1]
+            row["now_price"] = prices_temp[-1]
+            row["event_price"] = prices_temp[-1]
+            row["expiry_price"] = prices_temp[-1]
+            row["event_date"] = dates_temp[-1]
+            row["now_date"] = dates_temp[-1]
+            row["expiry_payoff"] = max(row["now_price"] - row["strike"], 0)
+            row["expiry_return"] = (prices_temp[-1] / prices_temp[0]) - 1
+            row["drawdown_return"] = np.amin(prices_temp) / prices_temp[0] - 1
+            row["duration"] = (row["event_date"] - row["spot_date"]).days / 365
+            row["r"] = r[-1]
+            row["q"] = q[-1]
+            row["v1"] = v1[-1]
+            row["v2"] = v2[-1]
+            row["t"] = t[-1]
+            stock_balance[-1] = 0
+            pnl = (stock_balance2 - stock_balance) * prices_temp
+            churn = (stock_balance2 - stock_balance)
+            row["pnl"] = np.nansum(pnl)
+            delta_churn = np.abs(churn)
+            row["delta_churn"] = np.nansum(delta_churn)
+            row["bot_return"] = row["pnl"] / prices_temp[0]
+            row["num_hedges"] = np.sum(stock_balance2 != stock_balance)
+
+        else:
+            # No event is triggered.
+            row.event = None
+            row["stock_balance"] = 0
+            row["stock_price"] = prices_temp[-1]
+            row["now_price"] = prices_temp[-1]
+            row["event_date"] = None
+            row["now_date"] = dates_temp[-1]
+            row["expiry_payoff"] = None
+            row["v1"] = v1[-1]
+            row["v2"] = v2[-1]
+            row["r"] = r[-1]
+            row["q"] = q[-1]
+            row["pnl"] = None
+            row["delta_churn"] = None
+            row["t"] = t[-1]
+            row["num_hedges"] = None
+        return row
+
+    logging.basicConfig(filename="logfilename.log", level=logging.INFO)
+
+    def fill_zeros_with_last(arr):
+        prev = np.arange(len(arr))
+        prev[arr == 2] = 2
+        prev = np.maximum.accumulate(prev)
+        return arr[prev]
+    def foo(k):
+        try:
+            # run the null filler for each section of dates
+            date_temp = dates_per_run[run_number][k]
+            null_df_small = null_df[null_df.spot_date == date_temp]
+            print(f"Filling {dates_per_run[run_number][k]}, {k} date from {len(dates_per_run[run_number])} dates.")
+            null_df_small = null_df_small.progress_apply(lambda x: exec_fill_fun(x, prices_np, dates_np, null_df), axis=1, raw=False)
+            null_df_small.drop(["expiry_date_index", "spot_date_index", "ticker_index"], axis=1, inplace=True)
+            null_df_small = null_df_small.infer_objects()
+            table_name = get_bot_uno_backtest_table_name()
+            if mod:
+                table_name += "_mod"
+            upsert_data_to_database(null_df_small, table_name, "uid", how="update", cpu_count=True, Text=True)
+            print(f"Finished {dates_per_run[run_number][k]}, {k} date from {len(dates_per_run[run_number])} dates.")
+
+        except Exception as e:
+            print(e)
+            logging.error(e)
+            logging.error(f"Error for {dates_per_run[run_number][k]}, {k} date from {len(dates_per_run[run_number])} dates.")
+            print(f"Error for {dates_per_run[run_number][k]}, {k} date from {len(dates_per_run[run_number])} dates.")
+
+
+    if len(null_df) > len(dates_per_run[run_number]):
+        for i in range(int(len(dates_per_run[run_number]))):
+            foo(i)
+    else:
+        if (history):
+            null_df["num_hedges"] = None
+        null_df = null_df.progress_apply(exec_fill_fun, axis=1, raw=True)
+        null_df.drop(["expiry_date_index", "spot_date_index", "ticker_index"], axis=1, inplace=True)
+        null_df = null_df.infer_objects()
+        if len(null_df) > 0 :
+            null_df = null_df.drop_duplicates(subset=["uid"], keep="first", inplace=False)
+            upsert_data_to_database(null_df, table_name, "uid", how="update", cpu_count=True, Text=True)
+    # ********************************************************************************************
+    print(f"Filling up the nulls is finished.")
+
