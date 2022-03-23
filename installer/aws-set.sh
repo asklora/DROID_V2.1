61d51fac72c90c4dd699f12f5e5d4297683d36b0
redloratech <rede.akbar@loratechai.com>
2022-03-23 12:26:56 +0700

    secret changes

--- installer/aws-set.sh
+++ installer/aws-set.sh
@@ -1,13 +1,13 @@
 if [ -x "$(command -v aws)" ]; then
-	aws configure set aws_access_key_id AKIA2XEOTUNGWEQ43TB6
-	aws configure set aws_secret_access_key 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
+	aws configure set aws_access_key_id AKIA2XEOTUNG3F3FQ2NW
+	aws configure set aws_secret_access_key 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
 	aws configure set default.region ap-east-1
 else
 	echo 'Error: aws is not installed.' >&2
 	echo 'Error: aws credentials not installed' >&2
 	apt-get install -yq awscli
-	aws configure set aws_access_key_id AKIA2XEOTUNGWEQ43TB6
-	aws configure set aws_secret_access_key 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
+	aws configure set aws_access_key_id AKIA2XEOTUNG3F3FQ2NW
+	aws configure set aws_secret_access_key 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
 	aws configure set default.region ap-east-1
 fi
 aws ecr get-login-password --region ap-east-1
